ccd233920b71167da9388c08d3bb88bbe06ab224
Jaime Gomes <jaime.gomes@protonmail.com>
2021-09-24 14:26:12 +0200

    added resource "azurerm_application_insights" "example" {

--- azure/appservice/main.tf
+++ azure/appservice/main.tf
@@ -43,3 +43,10 @@ resource "azurerm_app_service" "app" {
     squad       = var.squad
   }
 }
+
+resource "azurerm_application_insights" "apm" {
+  name                = "apm-${var.project}"
+  location            = azurerm_resource_group.app.location
+  resource_group_name = azurerm_resource_group.app.name
+  application_type    = "web"
+}
