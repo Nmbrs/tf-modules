b7e09b2ca2b0d008c6de779a6fc014bce4244bad
Jaime Gomes <jaime.gomes@protonmail.com>
2021-11-17 09:43:27 +0100

    removed the terraform template since it is using deprecated resources.

--- azure/windows-scaleset/main.tf
+++ azure/windows-scaleset/main.tf
@@ -15,10 +15,6 @@ data "azurerm_subnet" "scaleset" {
   resource_group_name  = var.vnet_resource_group_name
 }
 
-data "template_file" "tf-script" {
-  template = file("../terraform-modules//azure/windows-scaleset/script.ps1")
-}
-
 resource "azurerm_windows_virtual_machine_scale_set" "scaleset" {
   name                 = var.project
   computer_name_prefix = "vmss"
@@ -42,24 +38,6 @@ resource "azurerm_windows_virtual_machine_scale_set" "scaleset" {
     caching              = "ReadWrite"
   }
 
-/*
-  extension {
-    name                       = "CustomScript"
-    publisher                  = "Microsoft.Compute"
-    type                       = "CustomScriptExtension"
-    type_handler_version       = "1.10"
-    auto_upgrade_minor_version = true
-
-    settings = <<SETTINGS
-      {
-        "fileUris": ["https://raw.githubusercontent.com/Microsoft/dotnet-core-sample-templates/master/dotnet-core-music-windows/scripts/configure-music-app.ps1"],
-        "commandToExecute": "powershell -ExecutionPolicy Unrestricted -File configure-music-app.ps1"
-      }
-      SETTINGS
-      
-  }
-  */
-
   network_interface {
     name    = "vmss-${var.project}-nic"
     primary = true
@@ -79,9 +57,23 @@ resource "azurerm_virtual_machine_scale_set_extension" "extension" {
   type                 = "CustomScript"
   type_handler_version = "2.0"
 
+  settings = jsonencode({
+        "fileUris": ["https://raw.githubusercontent.com/Nmbrs/terraform-modules/workers-scaleset/scripts/worker-signal-extension.ps1"],
+        "commandToExecute": "powershell -ExecutionPolicy Unrestricted -File worker-signal-extension.ps1"
+
+    })    
+}
+
+/* resource "azurerm_virtual_machine_scale_set_extension" "extension1" {
+  name                 = "hostname"
+  virtual_machine_scale_set_id   = azurerm_windows_virtual_machine_scale_set.scaleset.id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
+
   settings = <<SETTINGS
     {
         "commandToExecute": "hostname && uptime"
     }
     SETTINGS
-}
+} */
