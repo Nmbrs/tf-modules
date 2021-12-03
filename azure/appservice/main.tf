65ae2765f4c039decfd6b2cca79f09ba40110f6d
Jaime Gomes <fargozhu@users.noreply.github.com>
2021-12-03 09:48:26 +0100

    fixed reserved state

--- azure/appservice/main.tf
+++ azure/appservice/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_app_service_plan" "app" {
   location            = azurerm_resource_group.app.location
   resource_group_name = azurerm_resource_group.app.name
   kind                = "Linux"
-  reserved            = false
+  reserved            = true
 
   sku {
     tier = var.plan
