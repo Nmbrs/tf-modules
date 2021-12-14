c42989d6932ea5059e7d4a5b40bc0edc53b64a3c
pmmluis <pedromluis@gmail.com>
2021-12-14 14:03:55 +0000

    removed randomizer

--- azure/nat-gateway/terraform.tf
+++ azure/nat-gateway/terraform.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 2.83.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
-    }
   }
 
   required_version = ">= 1.0.0"
