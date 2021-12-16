3be985459ed181db31298d672d9787028e97dd52
pmmluis <pedromluis@gmail.com>
2021-12-16 15:36:51 +0000

    changing the input variables

--- azure/nat-gateway/variables.tf
+++ azure/nat-gateway/variables.tf
@@ -13,11 +13,17 @@ variable "environment" {
   description = "defines the environment to provision the resources."
 }
 
+
 variable "vnet" {
-  type        = object({
-    resource_group_name   = string
-    virtual_network_name  = string
-    subnet_name           = string
-  })
-  description = "azure vnet structure data"
+  type = string
+  description = "subnet to be added to the nat gw"
 }
+
+# variable "vnet" {
+#   type        = object({
+#     resource_group_name   = string
+#     virtual_network_name  = string
+#     subnet_name           = string
+#   })
+#   description = "azure vnet structure data"
+# }
