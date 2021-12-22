de061ab836f747b2583c668fc52c6c508a416677
pmmluis <pedromluis@gmail.com>
2021-12-22 15:40:14 +0000

    changed resource group to use variable

--- azure/vnet/main.tf
+++ azure/vnet/main.tf
@@ -1,18 +1,14 @@
-data "azurerm_resource_group" "rg" {
-  name="rg-network"
-}
-
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.project}-${var.environment}"
   location            = local.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  resource_group_name = var.resource_group_name
   address_space       = ["${var.address_space}"]
 }
 
 resource "azurerm_subnet" "vnet" {
   for_each = var.subnet_prefixes
   name                 = each.key
-  resource_group_name  = data.azurerm_resource_group.rg.name
+  resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     =  [each.value]
 }
