4c7c0c49d9e7216e8c1e550c48dc2532669098ec
Filipe Figueredo Silva <filipe.silva@visma.com>
2022-02-24 14:27:35 +0000

    feat(storage-account): create storage account module (#40)
    * feat(*): add basic file structure
    
    * feat(*): resource group data
    
    * feat(*): add variables
    
    * feat(*): add tf providers
    
    * feat(*): add storage account logic
    
    * fix(*): remove duplicate variable
    
    * feat(*): update naming rule
    
    * docs(*): add README.md to storage account module
    
    * fix(*): fix storage account prefix to remove non alpha chars

---
+++ azure/storage_account/terraform.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.97.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
