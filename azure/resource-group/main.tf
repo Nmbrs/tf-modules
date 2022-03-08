6910a2862bb239119f8e2d095ffc16dd54864b0d
Jaime Gomes <jaime.gomes@visma.com>
2022-03-08 09:41:33 +0100

    maint: added new tag to the resource group module

--- azure/resource-group/main.tf
+++ azure/resource-group/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
   name     = "rg-${var.project}-${var.environment}"
   location = var.location
-  tags     = var.tags
+  tags = merge(var.tags, local.auto_tags)
 }
