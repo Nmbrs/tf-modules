0e8ecfb2c35723a182c5512231fe00609d976f36
Filipe Figueredo Silva <filipe.silva@visma.com>
2022-03-08 15:53:24 +0000

    feat(storage-account): add connection string and access key outputs

--- azure/storage_account/output.tf
+++ azure/storage_account/output.tf
@@ -1,7 +1,33 @@
-output "storage_account_name" {
+output "name" {
+  description = "The name of the Storage Account."
   value = azurerm_storage_account.storage_account.name
 }
 
-output "storage_account_id" {
+output "id" {
+  description = "The ID of the Storage Account."
   value = azurerm_storage_account.storage_account.id
+}
+
+output "primary_connection_string" {
+  description = "The connection string associated with the primary location."
+  value = azurerm_storage_account.storage_account.primary_connection_string
+  sensitive = true
+}
+
+output "secondary_connection_string" {
+  description = "The connection string associated with the secondary location."
+  value = azurerm_storage_account.storage_account.secondary_connection_string
+  sensitive = true
+}
+
+output "primary_access_key" {
+  description = "The primary access key for the storage account."
+  value = azurerm_storage_account.storage_account.primary_access_key
+  sensitive = true
+}
+
+output "secondary_access_key" {
+  description = "The secondary access key for the storage account."
+  value = azurerm_storage_account.storage_account.secondary_access_key
+  sensitive = true
 }
