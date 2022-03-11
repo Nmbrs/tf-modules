562d3cdb0f4e4bae9b6ef86f6332638874ac4ba1
pmmluis <pedromluis@gmail.com>
2022-03-11 04:48:55 +0000

    added TXT records

--- azure/custom-domain-binding/main.tf
+++ azure/custom-domain-binding/main.tf
@@ -4,12 +4,12 @@ data "azurerm_dns_zone" "binding" {
 }
 
 resource "azurerm_dns_cname_record" "binding" {
-  for_each            = var.app_name
-  name                = each.value
+  for_each            = var.apps
+  name                = each.value["name"]
   zone_name           = data.azurerm_dns_zone.binding.name
   resource_group_name = data.azurerm_dns_zone.binding.resource_group_name
   ttl                 = 300
-  record              = "${each.value}.azurewebsites.net"
+  record              = "${each.value["record"]}.azurewebsites.net"
 }
 
 data "azurerm_app_service" "binding" {
@@ -18,25 +18,24 @@ data "azurerm_app_service" "binding" {
   resource_group_name = var.resource_group
 }
 
-# resource "azurerm_dns_txt_record" "binding" {
-#   name                = "asuid.${azurerm_dns_cname_record.binding.name}"
-#   zone_name           = data.azurerm_dns_zone.binding.name
-#   resource_group_name = data.azurerm_dns_zone.binding.resource_group_name
-#   ttl                 = 300
-#   record {
-#     value = data.azurerm_app_service.binding.custom_domain_verification_id
-#   }
-# }
+resource "azurerm_dns_txt_record" "binding" {
+  for_each            = var.apps
+  name                = "asuid.${each.value["name"]}"
+  zone_name           = data.azurerm_dns_zone.binding.name
+  resource_group_name = data.azurerm_dns_zone.binding.resource_group_name
+  ttl                 = 300
+  record {
+    value = data.azurerm_app_service.binding[each.key].custom_domain_verification_id
+  }
+}
 
 resource "azurerm_app_service_custom_hostname_binding" "binding" {
   for_each            = var.apps
-  hostname            = trim(azurerm_dns_cname_record.binding[each.key].fqdn, ".")
+  hostname            = each.value["custom_domain"]
   app_service_name    = data.azurerm_app_service.binding[each.key].name
   resource_group_name = data.azurerm_app_service.binding[each.key].resource_group_name
-  #depends_on          = [azurerm_dns_txt_record.binding]
+  depends_on          = [azurerm_dns_txt_record.binding]
 
-  # Ignore ssl_state and thumbprint as they are managed using
-  # azurerm_app_service_certificate_binding.example
   lifecycle {
     ignore_changes = [ssl_state, thumbprint]
   }
