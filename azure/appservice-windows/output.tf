19c7a0924d9e53b6eea57794ecd3e2fc3c6f9ce7
pmmluis <pedromluis@gmail.com>
2022-03-11 04:51:22 +0000

    removal of output

--- azure/appservice-windows/output.tf
+++ azure/appservice-windows/output.tf
@@ -6,10 +6,6 @@ output "app_id" {
   value = azurerm_application_insights.app.app_id
 }
 
-# output "custom_domain" {
-#   value= {for k, domain in azurerm_app_service_custom_hostname_binding.custom_domain : k => domain.hostname}
-# }
-
 output "name" {
   value = {for k, app in azurerm_app_service.app : k => app.name}
 }
