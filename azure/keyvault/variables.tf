ab6b9d4c374c87abddf7ed464e14e17e9290141e
Filipe Figueredo Silva <filipe.silva@visma.com>
2022-03-16 13:54:35 +0000

    feat(keyvault): change permissions input variables into local variables (#70)
    * feat(keyvault): change permissions input variables into local variables
    
    * feat(keyvault): add output descriptions
    
    * fix(keyvault): fix read permissions to be set with proper values
    
    * fix(keyvault): fix read permissions to be set with proper values

--- azure/keyvault/variables.tf
+++ azure/keyvault/variables.tf
@@ -24,54 +24,11 @@ variable "tags" {
 }
 
 variable "writers" {
-  type = list(string)
+  type        = list(string)
   description = "Define a Azure Key Vault access policy"
 }
 
 variable "readers" {
-  type = list(string)
+  type        = list(string)
   description = "Define a Azure Key Vault access policy"
-}
-
-
-
-###################################################################################################
-# from this point forward we need to analyse which ones we need to expose to the user             #
-# or stream the multiple variables in one or two that are clear for the user.                     #
-###################################################################################################
-variable "kv_secret_permissions_full" {
-  type        = list(string)
-  description = "List of full secret permissions, must be one or more from the following: backup, delete, get, list, purge, recover, restore and set"
-  default     = ["backup", "delete", "get", "list", "purge", "recover", "restore", "set"]
-}
-
-variable "kv_certificate_permissions_full" {
-  type        = list(string)
-  description = "List of full certificate permissions, must be one or more from the following: backup, create, delete, deleteissuers, get, getissuers, import, list, listissuers, managecontacts, manageissuers, purge, recover, restore, setissuers and update"
-  default = ["create", "delete", "deleteissuers", "get", "getissuers", "import", "list", "listissuers",
-  "managecontacts", "manageissuers", "purge", "recover", "setissuers", "update", "backup", "restore"]
-}
-
-variable "kv_secret_permissions_read" {
-  type        = list(string)
-  description = "List of full secret permissions, must be one or more from the following: backup, delete, get, list, purge, recover, restore and set"
-  default     = ["get", "list"]
-}
-
-variable "kv_certificate_permissions_read" {
-  type        = list(string)
-  description = "List of full certificate permissions, must be one or more from the following: backup, create, delete, deleteissuers, get, getissuers, import, list, listissuers, managecontacts, manageissuers, purge, recover, restore, setissuers and update"
-  default     = ["get", "list"]
-}
-
-variable "kv_secret_permissions_write" {
-  type        = list(string)
-  description = "List of full secret permissions, must be one or more from the following: backup, delete, get, list, purge, recover, restore and set"
-  default     = ["get", "list", "set", "delete"]
-}
-
-variable "kv_certificate_permissions_write" {
-  type        = list(string)
-  description = "List of full certificate permissions, must be one or more from the following: backup, create, delete, deleteissuers, get, getissuers, import, list, listissuers, managecontacts, manageissuers, purge, recover, restore, setissuers and update"
-  default     = ["get", "list", "update", "delete"]
 }
