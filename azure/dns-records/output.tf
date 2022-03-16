28614a8ad50ca39da64c5ce3af35188094f09bab
pmmluis <pedromluis@gmail.com>
2022-03-16 13:39:48 +0000

    added outputs

--- azure/dns-records/output.tf
+++ azure/dns-records/output.tf
@@ -1,24 +1,11 @@
-# output "instrumentation_key" {
-#   value = azurerm_application_insights.app.instrumentation_key
-# }
+output "a_fqdn" {
+  value = azurerm_dns_a_record.record.fqdn
+}
 
-# output "app_id" {
-#   value = azurerm_application_insights.app.app_id
-# }
+output "cname_fqdn" {
+  value = azurerm_dns_cname_record.record.fqdn
+}
 
-# output "custom_domain" {
-#   value= {for k, domain in azurerm_app_service_custom_hostname_binding.custom_domain : k => domain.hostname}
-# }
-
-# output "dns_zone_id" {
-#   value = data.azurerm_dns_zone.binding
-# }
-
-# output "custom_domain_verification_id" {
-#   value = data.azurerm_app_service.binding.custom_domain_verification_id
-  
-# }
-
-# output "certificate_thumbprint" {
-#   value = data.azurerm_key_vault_certificate.binding.thumbprint
-# }
+output "txt_fqdn" {
+  value = azurerm_dns_txt_record.record.fqdn
+}
