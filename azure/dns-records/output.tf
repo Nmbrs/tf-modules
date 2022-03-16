842bfe698bfd0a45cca8c02b5c9e4bbe50e64d96
pmmluis <pedromluis@gmail.com>
2022-03-16 13:50:04 +0000

    fixed output due to the for_each

--- azure/dns-records/output.tf
+++ azure/dns-records/output.tf
@@ -1,11 +1,11 @@
 output "a_fqdn" {
-  value = azurerm_dns_a_record.record.fqdn
+  value = { for k, fqdn in azurerm_dns_a_record.record : k => fqdn.fqdn }
 }
 
 output "cname_fqdn" {
-  value = azurerm_dns_cname_record.record.fqdn
+  value = { for k, fqdn in azurerm_dns_cname_record.record : k => fqdn.fqdn }
 }
 
 output "txt_fqdn" {
-  value = azurerm_dns_txt_record.record.fqdn
+  value = { for k, fqdn in azurerm_dns_txt_record.record : k => fqdn.fqdn }
 }
