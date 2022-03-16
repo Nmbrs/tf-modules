5291d31897ec27dd7d00533d3996025642b5e42f
pmmluis <pedromluis@gmail.com>
2022-03-16 13:34:23 +0000

    Removed unnecessary things

--- azure/dns-records/local.tf
+++ azure/dns-records/local.tf
@@ -2,15 +2,4 @@ locals {
   auto_tags = {
     ManagedBy : "Terraform"
   }
-  # cname_record = [
-  #   for key, cname in var.cname : 
-  #   cname.record if ! contains([""], cname.record)
-  # ]
-  # cname_record = {
-  #   for_each = var.a
-  #   record = [for key, cname in var.cname :
-  #     cname.record if !contains([""], cname.record)
-  #   ]
-  # }
 }
-
