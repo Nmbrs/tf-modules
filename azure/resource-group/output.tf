34303ccef6066862a081d3e1d5ea36c44bcead98
Filipe Figueredo Silva <filipe.silva@visma.com>
2022-03-21 20:39:50 +0000

    feat(resource-group): add outputs description.

--- azure/resource-group/output.tf
+++ azure/resource-group/output.tf
@@ -1,15 +1,19 @@
 output "name" {
+  desdescription = "The Resource Group name."
   value = azurerm_resource_group.rg.name
 }
 
 output "id" {
+  description = "The ID of the Resource Group."
   value = azurerm_resource_group.rg.id
 }
 
 output "location" {
+  description = "The Azure Region where the Resource Group exists."
   value = azurerm_resource_group.rg.location
 }
 
 output "tags" {
+  description = "A mapping of tags assigned to the Resource Group."
   value = azurerm_resource_group.rg.tags
 }
