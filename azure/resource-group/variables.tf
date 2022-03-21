440684b7b62621dd7ac4b517c9d99988b6e3b050
Jaime Gomes <jaime.gomes@visma.com>
2022-03-21 22:12:49 +0100

    maint (rg): added variables required on tags

--- azure/resource-group/variables.tf
+++ azure/resource-group/variables.tf
@@ -3,14 +3,53 @@ variable "name" {
   type        = string
 }
 
-variable "tags" {
-  description = "Azure resource tags map."
-  type        = map(string)
+variable "product" {
+  description = "Name of the project to which the resources belongs."
+  type        = string
+
+  validation {
+    condition     = can(coalesce(var.product))
+    error_message = "The 'project' value is invalid. It must be a non-empty string."
+  }
 }
 
-variable "location" {
-  # For a complete list of available Azure regions run at cli:  
-  # az account list-locations  --query "[].{displayName:displayName, location:name}" --output table
-  description = "(Required) The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."
+variable "squad" {
+  description = "Name of the squad to which the resources belongs."
   type        = string
+
+  validation {
+    condition     = can(coalesce(var.squad))
+    error_message = "The 'squad' value is invalid. It must be a non-empty string."
+  }
+}
+
+variable "country" {
+  description = "Name of the contry to which the resources belongs."
+  type        = string
+
+  validation {
+    condition     = can(coalesce(var.country))
+    error_message = "The 'squad' value is invalid. It must be a non-empty string."
+  }
+}
+
+variable "environment" {
+  description = "The environment in which the resource should be provisioned."
+  type        = string
+
+  validation {
+    condition     = contains(["dev", "prod", "staging", "test", ], var.environment)
+    error_message = "The 'environment' value is invalid. Valid options are 'dev', 'prod','staging', 'test'."
+  }
+}
+
+variable "extra_tags" {
+  description = "A mapping of tags which should be assigned to the desired resource."
+  type        = map(string)
+  default     = {}
+
+  validation {
+    condition     = alltrue([for tag in var.extra_tags : can(coalesce(tag))])
+    error_message = "At least on tag value from 'tags' is invalid. They must be a non-empty string."
+  }
 }
