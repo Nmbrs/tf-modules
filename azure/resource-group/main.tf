2d60c7b23579167fef28348295c0f4c0a75d0ce4
Jaime Gomes <jaime.gomes@visma.com>
2022-03-21 22:13:11 +0100

    maint (rg): added caf naming convention provider

--- azure/resource-group/main.tf
+++ azure/resource-group/main.tf
@@ -1,5 +1,12 @@
+resource "azurecaf_name" "caf_name" {
+  name          = var.name
+  resource_type = "azurerm_resource_group"
+  suffixes      = [var.environment]
+  clean_input   = true
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = var.name
-  location = var.location
-  tags = merge(var.tags, local.auto_tags)
+  name     = azurecaf_name.caf_name.result
+  location = local.location
+  tags     = merge(var.extra_tags, local.default_tags)
 }
