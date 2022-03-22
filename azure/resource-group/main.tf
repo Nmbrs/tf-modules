b458c61a25b25d96acf7e410bf4c6af9e586a2c1
Jaime Gomes <jaime.gomes@visma.com>
2022-03-22 21:00:57 +0100

    maint (resource_group): added caf naming convention provider.

--- azure/resource-group/main.tf
+++ azure/resource-group/main.tf
@@ -1,12 +1,12 @@
 resource "azurecaf_name" "caf_name" {
-  name          = var.name
+  name          = lower(var.name)
   resource_type = "azurerm_resource_group"
-  suffixes      = [var.environment]
+  suffixes      = [lower(var.environment)]
   clean_input   = true
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = var.name
+  name     = azurecaf_name.caf_name.result
   location = var.location
   tags     = merge(var.extra_tags, local.default_tags)
 }
--- azure/resource-group/variables.tf
+++ azure/resource-group/variables.tf
@@ -71,34 +71,3 @@ variable "location" {
     error_message = "The 'location' value is invalid. Valid options are 'westeurope', 'northeurope'."
   }
 }
-
-variable "country" {
-  description = "Name of the contry to which the resources belongs."
-  type        = string
-
-  validation {
-    condition     = can(coalesce(var.country))
-    error_message = "The 'squad' value is invalid. It must be a non-empty string."
-  }
-}
-
-variable "environment" {
-  description = "The environment in which the resource should be provisioned."
-  type        = string
-
-  validation {
-    condition     = contains(["dev", "prod", "staging", "test", ], var.environment)
-    error_message = "The 'environment' value is invalid. Valid options are 'dev', 'prod','staging', 'test'."
-  }
-}
-
-variable "extra_tags" {
-  description = "A mapping of tags which should be assigned to the desired resource."
-  type        = map(string)
-  default     = {}
-
-  validation {
-    condition     = alltrue([for tag in var.extra_tags : can(coalesce(tag))])
-    error_message = "At least on tag value from 'tags' is invalid. They must be a non-empty string."
-  }
-}
