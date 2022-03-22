515d83c89f4d5ee6759bdfc772503b19994da805
Filipe Figueredo Silva <filipe.silva@visma.com>
2022-03-22 11:59:29 +0000

    ci(resource-group): update terraform code to comply with the cannonical identation

--- azure/resource-group/main.tf
+++ azure/resource-group/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
   name     = var.name
   location = var.location
-  tags = merge(var.extra_tags, local.default_tags)
+  tags     = merge(var.extra_tags, local.default_tags)
 }
