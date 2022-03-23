8426f1eee091afc973ba3fd22927e2edeb494970
pmmluis <pedromluis@gmail.com>
2022-03-23 01:36:31 +0000

    added outputs to the module

--- azure/dns-records/output.tf
+++ azure/dns-records/output.tf
@@ -12,3 +12,18 @@ output "txt_fqdn" {
   description = "The FQDN of the TXT record"
   value       = { for k, fqdn in azurerm_dns_txt_record.record : k => fqdn.fqdn }
 }
+
+output "a_record" {
+  description = "The FQDN of the A record"
+  value       = { for k, record in azurerm_dns_a_record.record : k => record.record }
+}
+
+output "cname_record" {
+  description = "The FQDN of the CNAME record"
+  value       = { for k, record in azurerm_dns_cname_record.record : k => record.record }
+}
+
+output "txt_record" {
+  description = "The FQDN of the TXT record"
+  value       = { for k, record in azurerm_dns_txt_record.record : k => record.record }
+}
