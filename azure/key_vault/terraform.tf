510890c82a59b898241deec145a2863d51e2f594
Filipe Figueredo Silva <filipe.silva@visma.com>
2022-05-11 08:59:14 +0100

    feature(key_vault): update azurerm and caf providers

--- azure/key_vault/terraform.tf
+++ azure/key_vault/terraform.tf
@@ -2,22 +2,34 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.98.0"
+      version = "~> 3.5.0"
     }
-
     azurecaf = {
       source  = "aztfmod/azurecaf"
-      version = "2.0.0-preview-2"
+      version = "2.0.0-preview-3"
     }
   }
 
   required_version = ">= 1.0.0"
 }
 
+
+
 provider "azurerm" {
   features {
     key_vault {
-      purge_soft_delete_on_destroy = true
+      purge_soft_delete_on_destroy               = true
+      purge_soft_deleted_certificates_on_destroy = true
+      purge_soft_deleted_keys_on_destroy         = true
+      purge_soft_deleted_secrets_on_destroy      = true
+
+      # When recovering soft-deleted Key Vault items (Keys, Certificates, and Secrets)
+      # the Principal used by Terraform needs the "recover" permission.
+      recover_soft_deleted_key_vaults   = true
+      recover_soft_deleted_certificates = true
+      recover_soft_deleted_keys         = true
+      recover_soft_deleted_secrets      = true
+
     }
   }
 }
