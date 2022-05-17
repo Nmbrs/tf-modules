efc0516d19c100324741e92e23460190a7d4e789
Filipe Figueredo Silva <filipe.silva@visma.com>
2022-05-17 08:30:27 +0100

    feature(resource_group): update azurerm and caf providers (#111)

--- azure/resource_group/terraform.tf
+++ azure/resource_group/terraform.tf
@@ -2,13 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.65"
+      version = "~> 3.5.0"
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
-      version = "2.0.0-preview-2"
+      version = "2.0.0-preview-3"
     }
   }
 
   required_version = ">= 1.0.0"
 }
+
