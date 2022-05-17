0e1e7ca1a7fb1aa8e16cc35d008589c034ebc864
Filipe Figueredo Silva <filipe.silva@visma.com>
2022-05-17 08:30:42 +0100

    build(key_vault): update azurerm and caf providers (#112)
    * feature(key_vault): update default policies to comply new validators
    
    * feature(key_vault): update azurerm and caf providers
    
    * docs(key_vault): update README to include the new kv reference

--- azure/key_vault/local.tf
+++ azure/key_vault/local.tf
@@ -9,7 +9,7 @@ locals {
   environment = var.environment
 
   secrets_full_permissions       = ["Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"]
-  certificates_full_permissions  = ["Backup", "Create", "Delete", "Deleteissuers", "Get", "Getissuers", "Import", "List", "Listissuers", "Managecontacts", "Manageissuers", "Purge", "Recover", "Restore", "Setissuers", "Update"]
+  certificates_full_permissions  = ["Backup", "Create", "Delete", "DeleteIssuers", "Get", "GetIssuers", "Import", "List", "ListIssuers", "ManageContacts", "ManageIssuers", "Purge", "Recover", "Restore", "SetIssuers", "Update"]
   secrets_read_permissions       = ["Get", "List"]
   certificates_read_permissions  = ["Get", "List"]
   secrets_write_permissions      = ["Get", "List", "Set", "Delete"]
