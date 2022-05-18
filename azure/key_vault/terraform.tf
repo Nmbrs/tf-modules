4910bf1e20cef61623cfcc5cf86e05b1bf0f143d
Filipe Figueredo Silva <filipe.silva@visma.com>
2022-05-18 07:54:38 +0100

    build: update providers versioning system

--- azure/key_vault/terraform.tf
+++ azure/key_vault/terraform.tf
@@ -2,15 +2,16 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.5.0"
+      version = "~> 3.6"
     }
+
     azurecaf = {
       source  = "aztfmod/azurecaf"
       version = "2.0.0-preview-3"
     }
   }
 
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.0.0, < 2.0.0"
 }
 
 
