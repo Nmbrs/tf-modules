ba91cac90249f3db856011bc8960f397428da4d0
Filipe Figueredo Silva <filipe.figueredo.ext@spms.min-saude.pt>
2022-07-21 08:51:17 +0100

    feature(vnet): update main.tf definition for vnet

--- azure/vnet/main.tf
+++ azure/vnet/main.tf
@@ -12,28 +12,28 @@ resource "azurerm_virtual_network" "vnet" {
   tags                = merge(local.default_tags, data.azurerm_resource_group.rg.tags, var.extra_tags)
 }
 
-# resource "azurerm_subnet" "subnet" {
-#   for_each = { for key, value in var.subnets : key => value }
-#   # for subnet in var.subnets : subnet.name => subnet }
+resource "azurerm_subnet" "subnet" {
+  for_each = { for key, value in var.subnets : key => value }
+  # for subnet in var.subnets : subnet.name => subnet }
 
-#   name                                           = each.value.name
-#   resource_group_name                            = data.azurerm_resource_group.rg.name
-#   virtual_network_name                           = azurerm_virtual_network.vnet.name
-#   address_prefix                                 = each.value.address_prefixes
-#   service_endpoints                              = lookup(each.value, "service_endpoints", [])
-#   enforce_private_link_service_network_policies  = lookup(each.value, "enforce_private_link_service_network_policies", false)
-#   enforce_private_link_endpoint_network_policies = lookup(each.value, "enforce_private_link_endpoint_network_policies", false)
+  name                                           = each.value.name
+  resource_group_name                            = data.azurerm_resource_group.rg.name
+  virtual_network_name                           = azurerm_virtual_network.vnet.name
+  address_prefixes                               = each.value.address_prefixes
+  service_endpoints                              = lookup(each.value, "service_endpoints", [])
+  enforce_private_link_service_network_policies  = lookup(each.value, "enforce_private_link_service_network_policies", false)
+  enforce_private_link_endpoint_network_policies = lookup(each.value, "enforce_private_link_endpoint_network_policies", false)
 
-  # dynamic "delegation" {
-  #   # for subnet in var.subnets : subnet.name => subnet }
-  #   for_each = lookup(each.value, "delegation", null) != null ? [""] : []
-  #   content {
-  #     name = each.value.delegation
-  #     service_delegation {
-  #       name    = each.value.delegation
-  #       actions = formatlist("Microsoft.Network/%s", local.service_delegation_actions[each.value.delegation])
+  #   dynamic "delegation" {
+  #     # for subnet in var.subnets : subnet.name => subnet }
+  #     for_each = lookup(each.value, "delegation", null) != null ? [""] : []
+  #     content {
+  #       name = each.value.delegation
+  #       service_delegation {
+  #         name    = each.value.delegation
+  #         actions = formatlist("Microsoft.Network/%s", local.service_delegation_actions[each.value.delegation])
+  #       }
   #     }
   #   }
-  # }
-# }
+}
 
