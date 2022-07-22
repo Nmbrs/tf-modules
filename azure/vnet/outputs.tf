24522ccbb63b8de0fcb5c3796020630f64ab2b85
Filipe Figueredo Silva <filipe.figueredo.ext@spms.min-saude.pt>
2022-07-22 10:28:00 +0100

    fix(vnet): update outputs

--- azure/vnet/outputs.tf
+++ azure/vnet/outputs.tf
@@ -8,7 +8,7 @@ output "id" {
   value       = azurerm_virtual_network.vnet.id
 }
 
-# output "subnet_ids" {
-#   description = "Contains a list of the the resource id of the subnets"
-#   value       = { for subnet in azurerm_subnet.subnet : subnet.name => subnet.id }
-# }
+output "subnet_ids" {
+  description = "Contains a list of the the resource id of the subnets"
+  value       = { for subnet in azurerm_subnet.subnet : subnet.name => subnet.id }
+}
