c6fe9e837f0f31e02964b7d504ee8f1b4bd47b0e
Filipe Figueredo Silva <filipe.silva@visma.com>
2022-07-26 16:42:08 +0100

    feat(repositories): add github repositories name uniqueness validation (#133)
    * feat(repository): add uniquiness validation to repository list
    
    * refactor(repository): update terraform.tf definition order
    
    * refactor(repositories): updated identation and name property readability
    
    * refactor(repositories): updated identation at main.tf
    
    Co-authored-by: Filipe Figueredo Silva <filipe@mac012671.lan>

--- github/repositories/terraform.tf
+++ github/repositories/terraform.tf
@@ -1,12 +1,12 @@
 terraform {
-  required_version = "~> 1.0"
-
   required_providers {
     github = {
       source  = "integrations/github"
       version = "~> 4.0"
     }
   }
+
+  required_version = "~> 1.0"
 }
 
 provider "github" {
