a64ba5d4cf2976e44829ba076df4daa2a00b3c37
Filipe Figueredo Silva <filipe.figueredo.silva@gmail.com>
2022-07-29 09:31:22 +0100

    feat(virtual_machine): add linux virtual machine support

--- azure/virtual_machine/main.tf
+++ azure/virtual_machine/main.tf
@@ -4,7 +4,7 @@ data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
 
-resource "azurecaf_name" "vm_windows_caf_name" {
+resource "azurecaf_name" "caf_name_windows_vm" {
   count         = locals.os_type == "windows" ? 1 : 0
   name          = lower(var.name)
   resource_type = "azurerm_windows_virtual_machine"
@@ -12,7 +12,7 @@ resource "azurecaf_name" "vm_windows_caf_name" {
   clean_input   = true
 }
 
-resource "azurecaf_name" "vm_linux_caf_name" {
+resource "azurecaf_name" "caf_name_linux_vm" {
   count         = locals.os_type == "linux" ? 1 : 0
   name          = lower(var.name)
   resource_type = "azurerm_linux_virtual_machine"
@@ -21,16 +21,62 @@ resource "azurecaf_name" "vm_linux_caf_name" {
 }
 
 
-resource "azurerm_network_interface" "nic" {
-  for_each            = { for nic in var.network_interfaces : nic.name => nic }
-  name                = each.value.name
+resource "random_string" "vm_unique_id" {
+  length  = 4
+  special = false
+  upper   = false
+
+  keepers = {
+    vm_name = lower(var.name)
+  }
+}
+
+resource "azurecaf_name" "caf_name_nic" {
+  name          = azurecaf_name.caf_name_vm.result
+  resource_type = "azurerm_network_interface"
+  suffixes      = [random_string.nic_unique_id.result]
+  clean_input   = true
+}
+
+resource "azurerm_network_interface" "main_nic" {
+  name                = azurecaf_name.caf_name_nic.result
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
+  tags                = data.azurerm_resource_group.rg.tags
 
   ip_configuration {
-    name                          = "main"
-    subnet_id                     = azurerm_subnet.example.id
+    name                          = "internal"
+    subnet_id                     = var.vnet_subnet_id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
+resource "azurerm_linux_virtual_machine" "linux_vm" {
+  count               = var.os_type == "linux" ? 1 : 0
+  name                = azurecaf_name.caf_name_vm.result
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  tags                = merge(local.default_tags, data.azurerm_resource_group.rg.tags, var.extra_tags)
+  size                = "Standard_D2s_v3"
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.main_nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = local.os_image[var.os_type].publisher
+    offer     = local.os_image[var.os_type].offer
+    sku       = local.os_image[var.os_type].sku
+    version   = local.os_image[var.os_type].version
+  }
+}
--- azure/virtual_machine/variables.tf
+++ azure/virtual_machine/variables.tf
@@ -8,6 +8,36 @@ variable "name" {
   type        = string
 }
 
+# variable "product" {
+#   description = "Name of the product to which the resources belongs."
+#   type        = string
+
+#   validation {
+#     condition     = can(coalesce(var.product))
+#     error_message = "The 'product' value is invalid. It must be a non-empty string."
+#   }
+# }
+
+# variable "squad" {
+#   description = "Name of the squad to which the resources belongs."
+#   type        = string
+
+#   validation {
+#     condition     = can(coalesce(var.squad))
+#     error_message = "The 'squad' value is invalid. It must be a non-empty string."
+#   }
+# }
+
+# variable "country" {
+#   description = "Name of the country to which the resources belongs."
+#   type        = string
+
+#   validation {
+#     condition     = can(coalesce(var.country))
+#     error_message = "The 'country' value is invalid. It must be a non-empty string."
+#   }
+# }
+
 variable "environment" {
   description = "The environment in which the resource should be provisioned."
   type        = string
@@ -27,15 +57,31 @@ variable "extra_tags" {
     condition     = alltrue([for tag in var.extra_tags : can(coalesce(tag))])
     error_message = "At least one tag value from 'extra_tags' is invalid. They must be non-empty string values."
   }
+}
 
-  variable "os_type" {
-    description = "The environment in which the resource should be provisioned."
-    type        = string
+variable "os_type" {
+  description = "The environment in which the resource should be provisioned."
+  type        = string
 
-    validation {
-      condition     = contains(["ubuntu 22.04 lts", "ubuntu 18.04 lts", "ubuntu 18.04 lts", "debian 10", "debian 11", "windows server 2016", "windows server 2019", "windows server 2022", "sql server 2017", "sql server 2019", "sql server 2022"], lower(var.os_type))
-      error_message = "The 'os_type' value is invalid. Valid options are 'ubuntu 22.04 lts', 'ubuntu 18.04 lts', 'ubuntu 18.04 lts', 'debian 10', 'debian 11', 'windows server 2016', 'windows server 2019', 'windows server 2022', 'sql server 2017', 'sql server 2019', 'sql server 2022'."
-    }
+  validation {
+    condition     = contains(["ubuntu 22.04 lts", "ubuntu 18.04 lts", "ubuntu 18.04 lts", "debian 10", "debian 11", "windows server 2016", "windows server 2019", "windows server 2022", "sql server 2017", "sql server 2019", "sql server 2022"], lower(var.os_type))
+    error_message = "The 'os_type' value is invalid. Valid options are 'ubuntu 22.04 lts', 'ubuntu 18.04 lts', 'ubuntu 18.04 lts', 'debian 10', 'debian 11', 'windows server 2016', 'windows server 2019', 'windows server 2022', 'sql server 2017', 'sql server 2019', 'sql server 2022'."
   }
+}
 
+variable "ssh_public_key" {
+  description = "Specifies the SSH public key used to access the virtual_machine."
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Specifies The administrator username for which the SSH Key should be configured."
+  type        = string
+  default     = "automation"
+}
+
+
+variable "vnet_subnet_id" {
+  description = "The ID of a Subnet where the Kubernetes Node Pool should exist. Changing this forces a new resource to be created."
+  type        = string
 }
