320a87b57b5ec9b56bf96edafaac3d26dbd924b1
Filipe Figueredo Silva <filipe.figueredo.silva@gmail.com>
2022-07-29 09:33:02 +0100

    feat(virtual_machine): add terraform random provider

--- azure/virtual_network/terraform.tf
+++ azure/virtual_network/terraform.tf
@@ -9,6 +9,11 @@ terraform {
       source  = "aztfmod/azurecaf"
       version = "2.0.0-preview-3"
     }
+
+        random = {
+      source  = "hashicorp/random"
+      version = "3.2.0"
+    }
   }
 
   required_version = ">= 1.0.0, < 2.0.0"
