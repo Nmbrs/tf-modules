4cfdaf44648f636673535946cf0e5fd21e4f89c6
Filipe Figueredo Silva <filipe.figueredo.silva@gmail.com>
2022-08-02 02:44:54 +0100

    feature(resource_group): ignore tags changes

--- azure/resource_group/local.tf
+++ azure/resource_group/local.tf
@@ -1,10 +1,7 @@
 locals {
   default_tags = {
-    ProvisionedBy = "Terraform"
-    Country       = var.country
-    Squad         = var.squad
-    Product       = var.product
-    Environment   = var.environment
+    environment = var.environment
+    managed_by  = "terraform"
+    status      = var.status
   }
-
 }
