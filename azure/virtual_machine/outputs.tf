0d74d48e8b1b0ce5432ccf9746ce9f7b117148e1
Filipe Figueredo Silva <filipe.figueredo.silva@gmail.com>
2022-08-02 04:54:20 +0100

    feat(virtual_machine): add name, os_type and os_image outputs

--- azure/virtual_machine/outputs.tf
+++ azure/virtual_machine/outputs.tf
@@ -1,4 +1,19 @@
 output "name" {
   description = "The virtual machine name."
-  value       = azurecaf_name.caf_name.result
+  value       = azurecaf_name.caf_name_linux_vm.result
+}
+
+output "os_type" {
+  description = "The virtual machine name."
+  value       = local.os_type
+}
+
+output "os_image" {
+  description = "The virtual machine name."
+  value = {
+    publisher = local.os_image[var.os_type].publisher
+    offer     = local.os_image[var.os_type].offer
+    sku       = local.os_image[var.os_type].sku
+    version   = local.os_image[var.os_type].version
+  }
 }
