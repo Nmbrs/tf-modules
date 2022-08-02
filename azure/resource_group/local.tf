3acc075742a4dcf45502f9d9f894e8b1507e8c87
Filipe Figueredo Silva <filipe.figueredo.silva@gmail.com>
2022-08-02 13:58:22 +0100

    feature(resource_group): update default_tags and input variables

--- azure/resource_group/local.tf
+++ azure/resource_group/local.tf
@@ -3,5 +3,9 @@ locals {
     environment = var.environment
     managed_by  = "terraform"
     status      = var.status
+    category    = var.category
+    owner       = var.owner
+    product     = var.product
+    country     = var.country
   }
 }
--- azure/resource_group/variables.tf
+++ azure/resource_group/variables.tf
@@ -8,58 +8,6 @@ variable "name" {
   }
 }
 
-# variable "product" {
-#   description = "Name of the product to which the resources belongs."
-#   type        = string
-
-#   validation {
-#     condition     = can(coalesce(var.product))
-#     error_message = "The 'product' value is invalid. It must be a non-empty string."
-#   }
-# }
-
-# variable "squad" {
-#   description = "Name of the squad to which the resources belongs."
-#   type        = string
-
-#   validation {
-#     condition     = can(coalesce(var.squad))
-#     error_message = "The 'squad' value is invalid. It must be a non-empty string."
-#   }
-# }
-
-# variable "country" {
-#   description = "Name of the country to which the resources belongs."
-#   type        = string
-
-#   validation {
-#     condition     = can(coalesce(var.country))
-#     error_message = "The 'country' value is invalid. It must be a non-empty string."
-#   }
-# }
-
-variable "environment" {
-  description = "The environment in which the resource should be provisioned."
-  type        = string
-  default     = "dev"
-
-  validation {
-    condition     = contains(["dev", "prod", "stag", "test", "sand"], var.environment)
-    error_message = "The 'environment' value is invalid. Valid options are 'dev', 'prod','stag', 'test', 'sand'."
-  }
-}
-
-variable "extra_tags" {
-  description = "(Optional) A extra mapping of tags which should be assigned to the desired resource."
-  type        = map(string)
-  default     = {}
-
-  validation {
-    condition     = alltrue([for tag in var.extra_tags : can(coalesce(var.extra_tags))])
-    error_message = "At least on tag value from 'extra_tags' is invalid. They must be non-empty string values."
-  }
-}
-
 variable "location" {
   # For a complete list of available Azure regions run at cli:  
   # az account list-locations  --query "[].{displayName:displayName, location:name}" --output table
@@ -73,6 +21,61 @@ variable "location" {
   }
 }
 
+variable "environment" {
+  description = "The environment in which the resource should be provisioned."
+  type        = string
+  default     = "dev"
+
+  validation {
+    condition     = contains(["dev", "prod", "stag", "test", "sand"], var.environment)
+    error_message = "The 'environment' value is invalid. Valid options are 'dev', 'prod','stag', 'test', 'sand'."
+  }
+}
+
+variable "product" {
+  description = "(Optional) Name of the product to which the resource belongs."
+  type        = string
+  default     = "not_applicable"
+
+  validation {
+    condition     = can(coalesce(var.product))
+    error_message = "The 'product' value is invalid. It must be a non-empty string."
+  }
+}
+
+variable "category" {
+  description = "(Optional) High level identification name that supports product componets."
+  type        = string
+  default     = "not_applicable"
+
+  validation {
+    condition     = can(coalesce(var.category))
+    error_message = "The 'categoy' value is invalid. It must be a non-empty string."
+  }
+}
+
+variable "owner" {
+  description = "(Optional) Name of the owner to which the resource belongs."
+  type        = string
+  default     = "not_applicable"
+
+  validation {
+    condition     = can(coalesce(var.owner))
+    error_message = "The 'owner' value is invalid. It must be a non-empty string."
+  }
+}
+
+variable "country" {
+  description = "(Optional) Name of the country to which the resources belongs."
+  type        = string
+  default     = "global"
+
+  validation {
+    condition     = contains(["global", "nl", "se"], var.country)
+    error_message = "The 'country' value is invalid. Valid options are 'global', 'nl','se'."
+  }
+}
+
 variable "status" {
   description = "(Optional) Indicates the resource state that can lead to post actions (either manually or automatically)."
   type        = string
@@ -81,5 +84,16 @@ variable "status" {
   validation {
     condition     = contains(["life_cycle", "not_compliant", "suspicious", "temporary", "to_delete", "to _review"], var.status)
     error_message = "The 'status' value is invalid. Valid options are 'life_cycle', 'not_compliant', 'suspicious', 'temporary', 'to_delete', 'to _review'."
+  }
+}
+
+variable "extra_tags" {
+  description = "(Optional) A extra mapping of tags which should be assigned to the desired resource."
+  type        = map(string)
+  default     = {}
+
+  validation {
+    condition     = alltrue([for tag in var.extra_tags : can(coalesce(var.extra_tags))])
+    error_message = "At least on tag value from 'extra_tags' is invalid. They must be non-empty string values."
   }
 }
