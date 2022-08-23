64e38c5479524801bf10981f16c4b0c93ddcd18c
Pedro Luis <84625586+pmmluis@users.noreply.github.com>
2022-08-23 12:34:21 +0100

    fix(provider): update modules (#146)
    * Update terraform modules providers.
    
    * fixed nat gateway zones

--- azure/nat_gateway/terraform.tf
+++ azure/nat_gateway/terraform.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.6"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
-    }
   }
 
   required_version = ">= 1.0.0"
