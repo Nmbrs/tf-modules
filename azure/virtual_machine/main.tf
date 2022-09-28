28d7abd7f8763b431a0ad3f07271b7d2e674da9a
Filipe Figueredo Silva <filipe.figueredo.silva@gmail.com>
2022-09-28 22:36:36 +0100

    feat(virtual_machine): implement virtual machine ssh and password generation

--- azure/virtual_machine/main.tf
+++ azure/virtual_machine/main.tf
@@ -4,22 +4,32 @@ data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
 
-resource "azurecaf_name" "caf_name_windows_vm" {
-  //count         = local.os_type == "windows" ? 1 : 0
+resource "azurecaf_name" "caf_name_vm" {
   name          = lower(var.name)
-  resource_type = "azurerm_windows_virtual_machine"
+  resource_type = "azurerm_virtual_machine"
   suffixes      = [lower(var.environment)]
   clean_input   = true
 }
 
-resource "azurecaf_name" "caf_name_linux_vm" {
-  //count         = 0 //local.os_type == "linux" ? 1 : 0
-  name          = lower(var.name)
-  resource_type = "azurerm_linux_virtual_machine"
-  suffixes      = [lower(var.environment)]
-  clean_input   = true
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = "4096"
 }
 
+resource "azurerm_ssh_public_key" "ssh_public_key" {
+  name                = azurecaf_name.caf_name_vm.result
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  tags                = data.azurerm_resource_group.rg.tags
+  public_key          = tls_private_key.ssh.public_key_openssh
+
+  lifecycle {
+    ignore_changes = [
+      tags["created_at"],
+      tags["updated_at"]
+    ]
+  }
+}
 
 resource "random_string" "nic_unique_id" {
   length  = 4
@@ -32,11 +42,20 @@ resource "random_string" "nic_unique_id" {
 }
 
 resource "azurecaf_name" "caf_name_nic" {
-  name          = azurecaf_name.caf_name_windows_vm.result
+  name          = azurecaf_name.caf_name_vm.result
   resource_type = "azurerm_network_interface"
   suffixes      = [random_string.nic_unique_id.result]
   clean_input   = true
 }
+
+resource "random_password" "password" {
+  length  = 32
+  special = true
+  keepers = {
+    vm_name = lower(var.name)
+  }
+}
+
 
 resource "azurerm_network_interface" "main_nic" {
   name                = azurecaf_name.caf_name_nic.result
@@ -49,11 +68,20 @@ resource "azurerm_network_interface" "main_nic" {
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
   }
+
+  lifecycle {
+    ignore_changes = [
+      tags["created_at"],
+      tags["updated_at"]
+    ]
+  }
 }
+
+
 
 resource "azurerm_linux_virtual_machine" "linux_vm" {
   count               = local.os_type == "linux" ? 1 : 0
-  name                = azurecaf_name.caf_name_linux_vm.result
+  name                = azurecaf_name.caf_name_vm.result
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   tags                = merge(local.default_tags, data.azurerm_resource_group.rg.tags, var.extra_tags)
@@ -65,7 +93,7 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = var.ssh_public_key
+    public_key = tls_private_key.ssh.public_key_openssh
   }
 
   os_disk {
@@ -78,5 +106,12 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
     offer     = local.os_image[var.os_type].offer
     sku       = local.os_image[var.os_type].sku
     version   = local.os_image[var.os_type].version
+  }
+
+  lifecycle {
+    ignore_changes = [
+      tags["created_at"],
+      tags["updated_at"]
+    ]
   }
 }
