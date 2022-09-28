9234171b220edf3277fe08aa2b6bb505de13c039
Filipe Figueredo Silva <filipe.figueredo.silva@gmail.com>
2022-09-28 22:35:26 +0100

    feat(virtual_machine): add random and tls providers

--- azure/virtual_machine/terraform.tf
+++ azure/virtual_machine/terraform.tf
@@ -9,6 +9,16 @@ terraform {
       source  = "aztfmod/azurecaf"
       version = "2.0.0-preview-3"
     }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.0"
+    }
+
+    random = {
+      source = "hashicorp/random"
+      version   = "~> 3.4.0"
+    }
   }
 
   required_version = ">= 1.0.0, < 2.0.0"
