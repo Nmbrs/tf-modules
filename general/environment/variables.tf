c3b3b318ea0047ad5ba0acdaff0dfb78cf5db05d
Filipe Figuredo Silva <filipe.silva@visma.com>
2023-01-17 23:15:05 +0000

    feat(environment): add stag environment

--- general/environment/variables.tf
+++ general/environment/variables.tf
@@ -3,8 +3,8 @@ variable "environment" {
   description = "The environment where the resources will be deployed. Acceptable values are 'dev', 'test', 'prod' or 'sand'."
 
   validation {
-    condition     = contains(["dev", "test", "prod", "sand"], var.environment)
-    error_message = format("Invalid value '%s' for variable 'environment', valid options are 'dev', 'test', 'prod', 'sand'.", var.environment)
+    condition     = contains(["dev", "test", "prod", "stag", "sand"], var.environment)
+    error_message = format("Invalid value '%s' for variable 'environment', valid options are 'dev', 'test', 'prod', 'stag', 'sand'.", var.environment)
   }
 }
 
