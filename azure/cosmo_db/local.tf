3992d230d506deda3c07865a0dcaa45ee95a4435
Divyesh Tandel <divyeshtandel1985@gmail.com>
2023-01-19 12:56:45 +0100

    fix: udpate the format

--- azure/cosmo_db/local.tf
+++ azure/cosmo_db/local.tf
@@ -1,5 +1,5 @@
 locals {
   default_tags = {
     managed_by = "terraform"
-  } 
+  }
 }
--- azure/cosmo_db/main.tf
+++ azure/cosmo_db/main.tf
@@ -5,24 +5,24 @@ data "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_cosmosdb_account" "cosmo_db" {
-  access_key_metadata_writes_enabled = true
-  analytical_storage_enabled         = false
-  default_identity_type              = "FirstPartyIdentity"
-  enable_automatic_failover          = false
-  enable_free_tier                   = false
-  enable_multiple_write_locations    = false
-  is_virtual_network_filter_enabled  = false
-  kind                               = var.kind 
-  local_authentication_disabled      = false
-  mongo_server_version                  = var.mongo_db_version 
-  name                                  = var.name             
+  access_key_metadata_writes_enabled    = true
+  analytical_storage_enabled            = false
+  default_identity_type                 = "FirstPartyIdentity"
+  enable_automatic_failover             = false
+  enable_free_tier                      = false
+  enable_multiple_write_locations       = false
+  is_virtual_network_filter_enabled     = false
+  kind                                  = var.kind
+  local_authentication_disabled         = false
+  mongo_server_version                  = var.mongo_db_version
+  name                                  = var.name
   network_acl_bypass_for_azure_services = false
   network_acl_bypass_ids                = []
   offer_type                            = "Standard"
   public_network_access_enabled         = true
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  tags = merge(data.azurerm_resource_group.rg.tags, local.default_tags, var.extra_tags)
+  location                              = data.azurerm_resource_group.rg.location
+  resource_group_name                   = data.azurerm_resource_group.rg.name
+  tags                                  = merge(data.azurerm_resource_group.rg.tags, local.default_tags, var.extra_tags)
 
   backup {
     interval_in_minutes = 240
