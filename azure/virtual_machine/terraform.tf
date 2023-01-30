6474541e4fcb9d51b4339a0b114b932a2b17f163
Filipe Figuredo Silva <filipe.silva@visma.com>
2023-01-30 09:54:49 +0000

    feat(virtual_machine): upgrade terraform providers

--- azure/virtual_machine/terraform.tf
+++ azure/virtual_machine/terraform.tf
@@ -7,21 +7,21 @@ terraform {
 
     azurecaf = {
       source  = "aztfmod/azurecaf"
-      version = "2.0.0-preview-3"
+      version = "2.0.0-preview3"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.0"
+      version = "~> 4.0.4"
     }
 
     random = {
       source = "hashicorp/random"
-      version   = "~> 3.4.0"
+      version   = "~> 3.2.0"
     }
   }
 
-  required_version = ">= 1.0.0, < 2.0.0"
+  required_version = ">= 1.3.0, < 2.0.0"
 }
 
 provider "azurerm" {
