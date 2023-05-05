ec7ad668a739ad15597305e057106236320c7c49
Pedro Luis <84625586+pmmluis@users.noreply.github.com>
2023-05-05 10:03:36 +0100

    Fix(natgateway): create nat gw module (#183)
    * fixed module to allow creating with multiple subnet associations and simplified subnet search
    
    * changed variable name
    
    * Creating and editing README
    
    * editing readme
    
    * changed readme and changed name of resource
    
    * changed the subnet variable to loop only through the name of the subnet, making it more error proof. Updated README
    
    * output description
    
    * added description to variable
    
    * updated README to fix a typo
    
    * terraform fmt
    
    ---------
    
    Co-authored-by: Filipe Figueredo Silva <filipe.figueredo.silva@gmail.com>

--- azure/nat_gateway/variables.tf
+++ azure/nat_gateway/variables.tf
@@ -1,20 +1,29 @@
-variable "project" {
+variable "name" {
   type        = string
-  description = "This variable defines the project name to be interpolated in multiple resources."
+  description = "This variable defines the name of the NAT gateway."
 }
 
 variable "natgw_resource_group" {
   type        = string
-  description = "nat gateway resource group name"
+  description = "Resource group name for where the NAT gateway will be created"
 }
 
 variable "environment" {
   type        = string
-  description = "defines the environment to provision the resources."
+  description = "Defines the environment to provision the resources."
 }
 
+variable "vnet_name" {
+  type        = string
+  description = "Name of the Vnet that will be added to the NAT gateway"
+}
 
-variable "vnet" {
+variable "vnet_resource_group" {
+  type        = string
+  description = "Resource group of the Vnet that will be added to the NAT gateway"
+}
+
+variable "subnets" {
   type        = list(any)
-  description = "subnet to be added to the nat gw"
+  description = "Subnets to be included in the NAT gateway"
 }
