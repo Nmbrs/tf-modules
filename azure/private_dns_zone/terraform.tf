11cea6356714c2c404ea8afe24e59030173cebc2
Filipe Figuredo Silva <filipe.silva@visma.com>
2023-05-17 11:05:48 +0100

    feat(private_dns_zone): add random provider to generate vnet links

--- azure/private_dns_zone/terraform.tf
+++ azure/private_dns_zone/terraform.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.6"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.2"
+    }
   }
 
   required_version = ">= 1.3.0, < 2.0.0"
@@ -13,3 +17,4 @@ provider "azurerm" {
   features {
   }
 }
+
