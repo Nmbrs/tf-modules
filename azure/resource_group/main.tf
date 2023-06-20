f686f6c4d136c2d03d83629e56ef3348c76855ed
Filipe Figuredo Silva <filipe.silva@visma.com>
2023-06-20 19:49:56 +0100

    feat(resource_group): remove caf and replace naming logic

--- azure/resource_group/main.tf
+++ azure/resource_group/main.tf
@@ -1,11 +1,5 @@
-resource "azurecaf_name" "caf_name" {
-  name          = lower(var.name)
-  resource_type = "azurerm_resource_group"
-  clean_input   = true
-}
-
 resource "azurerm_resource_group" "rg" {
-  name     = azurecaf_name.caf_name.result
+  name     = "rg-${var.name}-${var.environment}"
   location = var.location
   tags     = merge(local.default_tags, var.extra_tags)
 
