7e50b91e15a60dface58b7819a178dc143071769
Filipe Figueredo Silva <filipe.silva@visma.com>
2023-06-21 12:52:27 +0100

    feat(private_dns_resolver): add private dns resolver module (#189)
    * feat(private_dns_resolver): add private dns resolver module
    
    * feature(private_dns_resolver): replace random generated id in endpoints
    
    * docs(private_dns_resolver): update README with the new interface
    
    * chore(dns_resolver): lint code
    
    ---------
    
    Co-authored-by: Pedro Luis <84625586+pmmluis@users.noreply.github.com>

---
+++ azure/private_dns_resolver/main.tf
@@ -0,0 +1,57 @@
+data "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  resource_group_name = var.vnet_resource_group_name
+}
+
+resource "azurerm_private_dns_resolver" "resolver" {
+  location            = var.location
+  name                = "dnsres-${lower(var.name)}-${var.environment}"
+  resource_group_name = var.resource_group_name
+  virtual_network_id  = data.azurerm_virtual_network.vnet.id
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+data "azurerm_subnet" "inbound_endpoint" {
+  for_each             = toset(var.inbound_endpoints)
+  name                 = each.value
+  virtual_network_name = var.vnet_name
+  resource_group_name  = var.vnet_resource_group_name
+}
+
+resource "azurerm_private_dns_resolver_inbound_endpoint" "inbound_endpoint" {
+  for_each                = toset(var.inbound_endpoints)
+  name                    = each.value
+  private_dns_resolver_id = azurerm_private_dns_resolver.resolver.id
+  location                = azurerm_private_dns_resolver.resolver.location
+  ip_configurations {
+    private_ip_allocation_method = "Dynamic"
+    subnet_id                    = data.azurerm_subnet.inbound_endpoint[each.value].id
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+data "azurerm_subnet" "outbound_endpoint" {
+  for_each             = toset(var.outbound_endpoints)
+  name                 = each.value
+  virtual_network_name = var.vnet_name
+  resource_group_name  = var.vnet_resource_group_name
+}
+
+resource "azurerm_private_dns_resolver_outbound_endpoint" "outbound_endpoint" {
+  for_each                = toset(var.outbound_endpoints)
+  name                    = each.value
+  private_dns_resolver_id = azurerm_private_dns_resolver.resolver.id
+  location                = azurerm_private_dns_resolver.resolver.location
+  subnet_id               = data.azurerm_subnet.outbound_endpoint[each.value].id
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
---
+++ azure/private_dns_resolver/variables.tf
@@ -0,0 +1,60 @@
+variable "name" {
+  description = "Specifies the name which should be used for this Private DNS Resolver."
+  type        = string
+}
+
+variable "location" {
+  description = "The location where the resources will be deployed in Azure. For an exaustive list of locations, please use the command 'az account list-locations -o table'."
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment in which the resource should be provisioned."
+  type        = string
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Specifies the name of the Resource Group where the Private DNS Resolver should exist."
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "Specifies the name of the VNET associated with the Private DNS Resolver."
+}
+variable "vnet_resource_group_name" {
+  type        = string
+  description = "Specifies the name of the VNET name associated with the Private DNS Resolver."
+}
+
+variable "inbound_endpoints" {
+  description = "List of objects that represent the configuration of each inbound endpoint."
+  type        = list(string)
+  default     = []
+
+  validation {
+    condition     = alltrue([for endpoint in var.inbound_endpoints : can(coalesce(endpoint))])
+    error_message = "At least one element from the 'inbound_endpoints' list is invalid. They must be non-empty string values."
+  }
+
+  validation {
+    condition     = length(var.inbound_endpoints) == length(distinct(var.inbound_endpoints))
+    error_message = "At least one element from the 'inbound_endpoints' list is duplicated. They must be unique."
+  }
+}
+
+variable "outbound_endpoints" {
+  description = "List of objects that represent the configuration of each outbound endpoint."
+  type        = list(string)
+  default     = []
+
+  validation {
+    condition     = alltrue([for endpoint in var.outbound_endpoints : can(coalesce(endpoint))])
+    error_message = "At least one element from 'outbound_endpoints' list is invalid. They must be non-empty string values."
+  }
+
+  validation {
+    condition     = length(var.outbound_endpoints) == length(distinct(var.outbound_endpoints))
+    error_message = "At least one element from the 'outbound_endpoints' list is duplicated. They must be unique."
+  }
+}
