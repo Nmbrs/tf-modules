66aa0ff7375bfe6a374bdf11031bea22bfaea4e8
Filipe Figuredo Silva <filipe.silva@visma.com>
2023-08-17 20:16:36 +0100

    feat(app_service): update outputs

--- azure/app_service/output.tf
+++ azure/app_service/output.tf
@@ -1,7 +1,35 @@
-output "instrumentation_key" {
-  value = azurerm_application_insights.service_plan.instrumentation_key
+output "app_insights_instrumentation_key" {
+  description = "The Instrumentation Key for the Azure Application Insights associated with the service plan."
+  value       = azurerm_application_insights.service_plan.instrumentation_key
+  sensitive   = true
 }
 
-output "app_id" {
-  value = azurerm_application_insights.service_plan.app_id
+output "app_insights_id" {
+  description = "The Application ID (App ID) of the Azure Application Insights associated with the service plan."
+  value       = azurerm_application_insights.service_plan.app_id
 }
+
+output "service_plan_id" {
+  description = "The ID of the Azure Service Plan."
+  value       = azurerm_service_plan.service_plan.id
+}
+
+output "service_plan_os_type" {
+  description = "The operating system type associated with the Azure Service Plan."
+  value       = azurerm_service_plan.service_plan.os_type
+}
+
+output "service_plan_sku_name" {
+  description = "The SKU name associated with the Azure Service Plan."
+  value       = azurerm_service_plan.service_plan.sku_name
+}
+
+output "app_services" {
+  description = "List of Azure Windows Web Apps with their respective names and IDs."
+  value = [for app in azurerm_windows_web_app.web_app : {
+    name = app.name
+    id   = app.id
+  }]
+}
+
+
