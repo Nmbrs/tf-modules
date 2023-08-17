2fb922cf0b7d19d0a9e0e510f77021bcf5930c6f
Filipe Figuredo Silva <filipe.silva@visma.com>
2023-08-17 16:10:14 +0100

    chore(app_service): remove unused files

--- azure/app_service_windows/output.tf
+++
@@ -1,7 +0,0 @@
-output "instrumentation_key" {
-  value = azurerm_application_insights.service_plan.instrumentation_key
-}
-
-output "app_id" {
-  value = azurerm_application_insights.service_plan.app_id
-}
