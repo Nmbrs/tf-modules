407911f2e46f52d21f14dbf1160ed3fac9f708c2
Filipe Figuredo Silva <filipe.silva@visma.com>
2023-08-23 13:13:22 +0100

    feat(vpn_gateway): update ad authentication logic

--- azure/vpn_gateway/main.tf
+++ azure/vpn_gateway/main.tf
@@ -1,13 +1,17 @@
-data "azurerm_subnet" "vnet" {
+data "azurerm_client_config" "current" {}
+
+data "azurerm_subnet" "subnet" {
+  # The ID of the gateway subnet of a virtual network in which the virtual network gateway will be created. It is mandatory that the associated subnet is named GatewaySubnet. 
+  # Therefore, each virtual network can contain at most a single Virtual Network Gateway.
   name                 = "GatewaySubnet"
   virtual_network_name = var.vnet_name
-  resource_group_name  = var.vnet_resource_group
+  resource_group_name  = var.vnet_resource_group_name
 }
 
-resource "azurerm_public_ip" "vg" {
-  name                = "pip-vg-${var.name}-${var.environment}-ip"
+resource "azurerm_public_ip" "vpn_gateway" {
+  name                = "pip-vpng-${var.name}-${var.environment}"
   location            = var.location
-  resource_group_name = var.vg_resource_group
+  resource_group_name = var.resource_group_name
   allocation_method   = "Static"
   sku                 = "Standard"
 
@@ -16,29 +20,33 @@ resource "azurerm_public_ip" "vg" {
   }
 }
 
-resource "azurerm_virtual_network_gateway" "vg" {
-  name                = "vg-${var.name}-${var.environment}"
+resource "azurerm_virtual_network_gateway" "vpn_gateway" {
+  name                = "vpng-${var.name}-${var.environment}"
   location            = var.location
-  resource_group_name = var.vnet_resource_group
+  resource_group_name = var.vnet_resource_group_name
 
   type     = "Vpn"
   vpn_type = "RouteBased"
 
   active_active = false
   enable_bgp    = false
-  sku           = "VpnGw1"
+  sku           = var.sku
+  generation    = var.generation
 
   ip_configuration {
-    name                          = azurerm_public_ip.vg.name
-    public_ip_address_id          = azurerm_public_ip.vg.id
+    name                          = azurerm_public_ip.vpn_gateway.name
+    public_ip_address_id          = azurerm_public_ip.vpn_gateway.id
     private_ip_address_allocation = "Dynamic"
-    subnet_id                     = data.azurerm_subnet.vnet.id
+    subnet_id                     = data.azurerm_subnet.subnet.id
   }
+
   vpn_client_configuration {
     address_space        = var.address_space
     vpn_client_protocols = ["OpenVPN"]
-    aad_tenant           = var.aad_tenant
-    aad_audience         = var.aad_audience
-    aad_issuer           = var.aad_issuer
+
+    # Learn more about Azure AD authentication https://learn.microsoft.com/en-us/azure/vpn-gateway/openvpn-azure-ad-tenant
+    aad_tenant   = local.aad_tenant_url
+    aad_audience = local.vpn_enterprise_app_id
+    aad_issuer   = local.aad_issuer_url
   }
 }
