10d881a6b4df2f9a0c8b183871b74751faad2faa
Filipe Figuredo Silva <filipe.silva@visma.com>
2023-08-28 09:47:31 +0100

    feature(service_bus): add environment variable, add sku and capacity validations

--- azure/service_bus/variables.tf
+++ azure/service_bus/variables.tf
@@ -1,26 +1,40 @@
+variable "resource_group_name" {
+  type        = string
+  description = "The name of an existing Resource Group."
+}
+
 variable "name" {
-  description = "The name of the namespace."
+  description = "The name of the service bus namespace."
   type        = string
 }
 
-variable "resource_group_name" {
-  description = "The name of the rg."
+variable "environment" {
+  description = "The environment in which the resource should be provisioned."
   type        = string
 }
 
 variable "location" {
-  description = "The name of the location."
+  description = "The location where the resources will be deployed in Azure. For an exaustive list of locations, please use the command 'az account list-locations -o table'."
   type        = string
 }
 
 variable "sku" {
-  description = "The SKU of the namespace. The options are: `Basic`, `Standard`, `Premium`."
+  description = "Configuration of the size and capacity of the virtual network gateway."
   type        = string
-  default     = "Standard"
+
+  validation {
+    condition     = contains(["Basic", "Standard", "Premium"], var.sku)
+    error_message = format("Invalid value '%s' for variable 'sku', valid options are 'Basic', 'Standard', 'Premium'.", var.sku)
+  }
 }
 
 variable "capacity" {
-  description = "The number of message units."
+  description = "The number of message units (resource isolation at the CPU and memory level so that each customer workload runs in isolation)."
   type        = number
   default     = 0
+
+  validation {
+    condition     = contains([0, 1, 2, 4, 8, 16], var.capacity)
+    error_message = format("Invalid value '%s' for variable 'capacity', valid options are 0, 1, 2, 4, 8, 16.", var.capacity)
+  }
 }
