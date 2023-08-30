cb98ecdba3628223bd72876927ee78bab120c91b
Filipe Figuredo Silva <filipe.silva@visma.com>
2023-08-30 16:30:00 +0100

    ci(app_service): update terraform providers version

--- azure/app_service/terraform.tf
+++ azure/app_service/terraform.tf
@@ -2,8 +2,9 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.6"
+      version = "~> 3.70"
     }
   }
-  required_version = ">= 1.3.0"
+
+  required_version = ">= 1.5.0, < 2.0.0"
 }
