2e78afee3ae254bd9677b82da4f965a51009fc2e
Filipe Figuredo Silva <filipe.silva@visma.com>
2023-09-06 16:03:43 +0100

    feature(vpn_gateway): remove current subscription data block

--- azure/vpn_gateway/main.tf
+++ azure/vpn_gateway/main.tf
@@ -1,5 +1,3 @@
-data "azurerm_client_config" "current" {}
-
 data "azurerm_subnet" "subnet" {
   # The ID of the gateway subnet of a virtual network in which the virtual network gateway will be created. It is mandatory that the associated subnet is named GatewaySubnet. 
   # Therefore, each virtual network can contain at most a single Virtual Network Gateway.
