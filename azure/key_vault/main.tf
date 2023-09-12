37b2a0ca751f75eac2203c1c229eaf9b33735b6b
Filipe Figuredo Silva <filipe.silva@visma.com>
2023-09-12 15:05:40 +0100

    feature!(key_vault): remove access permissions for testing purpose

--- azure/key_vault/main.tf
+++ azure/key_vault/main.tf
@@ -1,4 +1,4 @@
-data "azurerm_client_config" "current" {}
+# data "azurerm_client_config" "current" {}
 
 resource "azurecaf_name" "caf_name" {
   name          = lower("${local.org}-${var.name}")
@@ -29,47 +29,47 @@ resource "azurerm_key_vault" "key_vault" {
   }
 }
 
-# Create a Default Azure Key Vault access policy with Admin permissions
-# This policy must be kept for a proper run of the "destroy" process
-resource "azurerm_key_vault_access_policy" "default_policy" {
-  key_vault_id = azurerm_key_vault.key_vault.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
+# # Create a Default Azure Key Vault access policy with Admin permissions
+# # This policy must be kept for a proper run of the "destroy" process
+# resource "azurerm_key_vault_access_policy" "default_policy" {
+#   key_vault_id = azurerm_key_vault.key_vault.id
+#   tenant_id    = data.azurerm_client_config.current.tenant_id
+#   object_id    = data.azurerm_client_config.current.object_id
 
-  certificate_permissions = local.certificates_full_permissions
-  key_permissions         = local.keys_full_permissions
-  secret_permissions      = local.secrets_full_permissions
-  storage_permissions     = local.storage_full_permissions
+#   certificate_permissions = local.certificates_full_permissions
+#   key_permissions         = local.keys_full_permissions
+#   secret_permissions      = local.secrets_full_permissions
+#   storage_permissions     = local.storage_full_permissions
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-resource "azurerm_key_vault_access_policy" "readers_policy" {
-  for_each = {
-    for policy in var.policies : trimspace(lower(policy.name)) => policy
-    if policy.type == "readers"
-  }
-  key_vault_id            = azurerm_key_vault.key_vault.id
-  tenant_id               = data.azurerm_client_config.current.tenant_id
-  object_id               = each.value.object_id
-  certificate_permissions = local.certificates_read_permissions
-  key_permissions         = local.keys_read_permissions
-  secret_permissions      = local.secrets_read_permissions
-  storage_permissions     = local.storage_read_permissions
-}
+# resource "azurerm_key_vault_access_policy" "readers_policy" {
+#   for_each = {
+#     for policy in var.policies : trimspace(lower(policy.name)) => policy
+#     if policy.type == "readers"
+#   }
+#   key_vault_id            = azurerm_key_vault.key_vault.id
+#   tenant_id               = data.azurerm_client_config.current.tenant_id
+#   object_id               = each.value.object_id
+#   certificate_permissions = local.certificates_read_permissions
+#   key_permissions         = local.keys_read_permissions
+#   secret_permissions      = local.secrets_read_permissions
+#   storage_permissions     = local.storage_read_permissions
+# }
 
-resource "azurerm_key_vault_access_policy" "writers_policy" {
-  for_each = {
-    for policy in var.policies : trimspace(lower(policy.name)) => policy
-    if policy.type == "writers"
-  }
-  key_vault_id            = azurerm_key_vault.key_vault.id
-  tenant_id               = data.azurerm_client_config.current.tenant_id
-  object_id               = each.value.object_id
-  certificate_permissions = local.certificates_write_permissions
-  key_permissions         = local.keys_write_permissions
-  secret_permissions      = local.secrets_write_permissions
-  storage_permissions     = local.storage_write_permissions
-}
+# resource "azurerm_key_vault_access_policy" "writers_policy" {
+#   for_each = {
+#     for policy in var.policies : trimspace(lower(policy.name)) => policy
+#     if policy.type == "writers"
+#   }
+#   key_vault_id            = azurerm_key_vault.key_vault.id
+#   tenant_id               = data.azurerm_client_config.current.tenant_id
+#   object_id               = each.value.object_id
+#   certificate_permissions = local.certificates_write_permissions
+#   key_permissions         = local.keys_write_permissions
+#   secret_permissions      = local.secrets_write_permissions
+#   storage_permissions     = local.storage_write_permissions
+# }
