20aa7736e0553e277cfdda47534903a5b9454de3
Filipe Figueredo Silva <filipe.silva@visma.com>
2023-09-13 16:11:47 +0100

    fix(service_bus): update capacity validations for all tiers (#237)
    * fix(service_bus): update capacity validations for all tiers
    
    * refactor(service_bus): update preconditions

--- azure/service_bus/main.tf
+++ azure/service_bus/main.tf
@@ -14,8 +14,18 @@ resource "azurerm_servicebus_namespace" "service_bus" {
     ignore_changes = [tags]
 
     precondition {
-      condition     = (var.sku_name == "Basic" && var.capacity == 0) || (var.sku_name == "Standard" && var.capacity == 0) || var.sku_name == "Premium"
-      error_message = "The capacity (message unit) must be 0 when using the 'Basic' or 'Standard' SKU."
+      condition     = (var.sku_name == "Basic" && var.capacity == 0) || var.sku_name == "Standard" || var.sku_name == "Premium"
+      error_message = format("Invalid value '%s' for variable 'capacity' (message units) when using the 'Basic' SKU, it must be 0.", var.capacity)
+    }
+
+    precondition {
+      condition     = (var.sku_name == "Standard" && var.capacity == 0) || var.sku_name == "Basic" || var.sku_name == "Premium"
+      error_message = format("Invalid value '%s' for variable 'capacity' (message units) when using the 'Standard' SKU, it must be 0.", var.capacity)
+    }
+
+    precondition {
+      condition     = (var.sku_name == "Premium" && contains([1, 2, 4, 8, 16], var.capacity)) || var.sku_name == "Basic" || var.sku_name == "Standard"
+      error_message = format("Invalid value '%s' for variable 'capacity' (message units) when using the 'Premium' SKU, valid options are 1, 2, 4, 8, 16.", var.capacity)
     }
   }
 }
