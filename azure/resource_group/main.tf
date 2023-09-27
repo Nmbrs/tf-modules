3eeff8e8bb2d3a7545acf32e42d569bd1c2295e9
Filipe Figueredo Silva <filipe.silva@visma.com>
2023-09-27 16:29:10 +0100

    feature(resource_group): remove mandatory tags (#243)
    * feature(resource_group): remove mandatory tags
    
    * Update azure/resource_group/variables.tf
    
    Co-authored-by: Pedro Luis <84625586+pmmluis@users.noreply.github.com>
    
    * feature(resource_group): update README.md
    
    * feature(resource_group): remove README.md1
    
    ---------
    
    Co-authored-by: Pedro Luis <84625586+pmmluis@users.noreply.github.com>

--- azure/resource_group/main.tf
+++ azure/resource_group/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "rg" {
   name     = "rg-${var.name}-${var.environment}"
   location = var.location
-  tags     = merge(local.default_tags, var.extra_tags)
+  tags     = var.tags
 
   lifecycle {
     ignore_changes = [tags]
