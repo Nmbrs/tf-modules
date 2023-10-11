fa106fc158758e6bb42707d386c795e2774de6cc
Filipe Figuredo Silva <filipe.silva@visma.com>
2023-10-11 20:11:45 +0100

    feature(private_dns_resolver): update vnet link resource group

--- azure/private_dns_zone/terraform.tf
+++ azure/private_dns_zone/terraform.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.70"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5"
-    }
   }
 
   required_version = ">= 1.5.0, < 2.0.0"
