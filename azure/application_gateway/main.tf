939f0484ce95dc03a53f2d707c2b467528c47a3e
Filipe Figuredo Silva <filipe.silva@visma.com>
2023-11-23 11:54:51 +0000

    refactor(application_gateway): remove zone definiton for the public ip

--- azure/application_gateway/main.tf
+++ azure/application_gateway/main.tf
@@ -8,7 +8,6 @@ resource "azurerm_public_ip" "app_gw" {
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
   sku                 = "Standard"
-  zones               = ["1"]
 
   lifecycle {
     ignore_changes = [tags]
