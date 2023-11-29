8d8ecd3cb1e37261983e34de28df7ccf168d7794
Filipe Figueredo Silva <filipe.silva@visma.com>
2023-11-29 14:16:28 +0000

    feature(nat_gateway): replace name parameter in favor of  workload (#260)
    Co-authored-by: Pedro Luis <84625586+pmmluis@users.noreply.github.com>

--- azure/nat_gateway/output.tf
+++ azure/nat_gateway/output.tf
@@ -1,3 +1,18 @@
+output "name" {
+  description = "The NAT gateway full name."
+  value       = azurerm_nat_gateway.natgw.name
+}
+
+output "workload" {
+  description = "The NAT gateway workload name."
+  value       = var.workload
+}
+
+output "id" {
+  description = "The NAT gateway  ID."
+  value       = azurerm_nat_gateway.natgw.id
+}
+
 output "public_ip_address" {
   value       = azurerm_public_ip.natgw.ip_address
   description = "Output of the public IP"
