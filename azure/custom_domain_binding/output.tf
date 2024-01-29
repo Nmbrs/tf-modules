5b37080604153b102782418a8b2fb290d72661e6
Filipe Figueredo Silva <filipe.silva@visma.com>
2024-01-29 16:52:49 +0000

    feature(custom_domain_binding): remove custom_domain_binding module (#218)
    Co-authored-by: Pedro Luis <84625586+pmmluis@users.noreply.github.com>

--- azure/custom_domain_binding/output.tf
+++
@@ -1,7 +0,0 @@
-output "custom_domain" {
-  value = { for k, domain in azurerm_app_service_custom_hostname_binding.binding : k => domain.hostname }
-}
-
-output "certificate_thumbprint" {
-  value = data.azurerm_key_vault_certificate.binding.thumbprint
-}
