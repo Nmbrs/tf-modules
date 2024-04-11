26ebe802a9ba197199b1569345c0c46a86daf922
pmmluis <pedromluis@gmail.com>
2024-04-11 14:28:37 +0100

    fixed typo

--- azure/sql_database/main.tf
+++ azure/sql_database/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_mssql_database" "sql_database" {
   name            = var.override_name != "" ? var.override_name : local.sql_database_name
   server_id       = data.azurerm_mssql_server.sql_server.id
-  sku_name        = var.sql_elastic_pool_name != "" ? : var.sku_name
+  sku_name        = var.sql_elastic_pool_name != "" ? null : var.sku_name
   collation       = var.collation
   license_type    = var.license_type
   elastic_pool_id = var.sql_elastic_pool_name != "" ? data.azurerm_mssql_elasticpool.sql_elasticpool.id : null
