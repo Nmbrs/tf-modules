d73cf0101c0465d192caa203b1b26d91ee046e97
Filipe Figuredo Silva <filipe.silva@visma.com>
2024-05-29 18:47:46 +0100

    feature(private_endpoint): add network interface name

--- azure/private_endpoint/main.tf
+++ azure/private_endpoint/main.tf
@@ -1,8 +1,9 @@
 resource "azurerm_private_endpoint" "endpoint" {
-  name                = local.private_endpoint_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  subnet_id           = data.azurerm_subnet.subnet.id
+  name                          = local.private_endpoint_name
+  location                      = var.location
+  resource_group_name           = var.resource_group_name
+  subnet_id                     = data.azurerm_subnet.subnet.id
+  custom_network_interface_name = "nic-${local.private_endpoint_name}"
 
   dynamic "private_service_connection" {
     for_each = local.resource_data_blocks[var.resource_type] != null ? [1] : []
@@ -13,6 +14,7 @@ resource "azurerm_private_endpoint" "endpoint" {
       is_manual_connection           = false
     }
   }
+
   private_dns_zone_group {
     name                 = data.azurerm_private_dns_zone.private_dns_zone.name
     private_dns_zone_ids = tolist([data.azurerm_private_dns_zone.private_dns_zone.id])
