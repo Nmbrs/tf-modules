e5e8bdf2570169af161ba72ad7c41235c5d5b849
Filipe Figuredo Silva <filipe.silva@visma.com>
2024-06-06 10:41:11 +0100

    refactor(private_endpoint): update naming logic

--- azure/private_endpoint/local.tf
+++ azure/private_endpoint/local.tf
@@ -1,5 +1,5 @@
 locals {
-  private_endpoint_name = "pep-${var.workload}-${var.location}-${var.environment}-${format("%03d", var.instance_count)}"
+  private_endpoint_name = "pep-${var.workload}-${var.location}-${format("%03d", var.instance_count)}"
   resource_data_blocks = {
     app_service           = data.azurerm_windows_web_app.app_service,
     storage_account_blob  = data.azurerm_storage_account.storage_account_blob,
