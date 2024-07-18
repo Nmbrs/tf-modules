0a98c859a95f7124e7a3d5780a20de0dead493b1
Filipe Figuredo Silva <filipe.silva@visma.com>
2024-07-18 17:38:06 +0100

    feature(container): create containers

--- azure/container_app_environment/local.tf
+++ azure/container_app_environment/local.tf
@@ -1,5 +1,5 @@
 locals {
 #container_app_environment_name = lower("cae-${var.workload}-${var.environment}-${var.location}-${format("%03d", var.naming_count)}")
-container_app_environment_name = lower("cae-${var.workload}-${var.environment}-${format("%03d", var.naming_count)}")
+container_app_environment_name = lower("cae-${var.workload}-${format("%03d", var.naming_count)}-${var.environment}")
 #container_app_environment_name = lower("cae-${var.workload}-${var.environment}")
 }
