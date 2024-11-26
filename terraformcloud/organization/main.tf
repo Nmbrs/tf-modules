52f449c7c1934a9a7697b134fc7ce5da59db65dd
Filipe Figueredo Silva <filipe.silva@visma.com>
2024-11-26 20:45:43 +0000

    feature(terraformcloud_organization): update variables logic

--- terraformcloud/organization/main.tf
+++ terraformcloud/organization/main.tf
@@ -1,7 +1,7 @@
 resource "tfe_organization" "organization" {
-  name                          = var.name
-  email                         = var.admin_email
-  allow_force_delete_workspaces = true
-  collaborator_auth_policy      = "two_factor_mandatory"
+  name                                = var.name
+  email                               = var.admin_email
+  allow_force_delete_workspaces       = true
+  collaborator_auth_policy            = "two_factor_mandatory"
   speculative_plan_management_enabled = false
 }
