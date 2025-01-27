329aaf661e0cf62d2d1c9ad6c202c8509a7468c3
Filipe Figueredo Silva <filipe.silva@visma.com>
2025-01-27 16:58:56 +0000

    feature(cdn_frontdoor): add module (#313)
    * feature(cdn_frontdoor): add module
    
    * feature(cdn_frontdoor): add endpoint logic
    
    * fix(cdn_frontdoor): update response timeout validation logic
    
    * feature(cdn_frontdoor): add origin and origin groups
    
    * fix(cdn_frontdoor): updated the reference to session affinity
    
    * fix(cdn_frontdoor): add title function to refer to http and https protocols
    
    * feature(cdn_frontdoor): add a logic to name origins
    
    * feature(cdn_frontdoor): add lifecycle logic to ignore tags
    
    * feature(cdn_frontdoor): add lifecycle logic to ignore tags
    
    * feature(cdn_frontdoor): add lifecycle logic to ignore tags
    
    * feature(cdn_frontdoor): add initial route logic
    
    * feature(cdn_frontdoor): add path regex
    
    * feature(cdn_frontdoor): add ruleset
    
    * feature(cdn_frontdoor): add ruleset association logic
    
    * feature(cdn_frontdoor): add custom_domain_logic
    
    * fix(cdn_frontdoor): fix for each loop logic for custom domains
    
    * fix(cdn_frontdoor): fix naming logic for domains
    
    * feature(cdn_frontdoor): add route association logic for custom domain
    
    * feature(cdn_frontdoor): update endpoint health probe and origin logic
    
    * feature(cdn_frontdoor): update endpoint health probe and origin logic
    
    * feature(cdn_frontdoor): update endpoint health probe and origin logic
    
    * feature(cdn_frontdoor): update endpoint health probe and origin logic
    
    * feature(cdn_frontdoor): update rule logic
    
    * feature(cdn_frontdoor): add code to support custom domains
    
    * feature(cdn_frontdoor): add code to support custom domains
    
    * feature(cdn_frontdoor): add code to support custom domains
    
    * feature(cdn_frontdoor): add code to support custom domains
    
    * feature(cdn_frontdoor): add code to support custom domains
    
    * feature(cdn_frontdoor): remove route local var
    
    * feature(cdn_frontdoor): remove route local var
    
    * feature(cdn_frontdoor): remove route local var
    
    * feature(cdn_frontdoor): remove cname logic
    
    * feature(cdn_frontdoor): add compact function to remove null from arrays
    
    * fix(cdn_frontdoor): update cdn priority
    
    * docs(cdn_frontdoor): add README
    
    * feature(cdn_frontdoor): increase health probe latency

---
+++ azure/cdn_frontdoor/local.tf
@@ -0,0 +1,32 @@
+locals {
+  frontdoor_profile_name = lower("afd-${var.workload}-${var.environment}")
+
+  sku_name = {
+    Standard = "Standard_AzureFrontDoor"
+    Premium  = "Premium_AzureFrontDoor"
+  }
+
+  origins = flatten([
+    for endpoint in var.endpoints : [
+      for fqdn in endpoint.origin_settings.fqdns :
+      {
+        associated_endpoint_name = endpoint.name
+        fqdn                     = fqdn
+        path                     = endpoint.origin_settings.path
+        session_affinity_enabled = endpoint.origin_settings.session_affinity_enabled
+      }
+    ]
+  ])
+
+  custom_domains = flatten([
+    for endpoint in var.endpoints : [
+      for domain in endpoint.custom_domains :
+      {
+        fqdn                         = domain.fqdn
+        dns_zone_name                = domain.dns_zone_name
+        dns_zone_resource_group_name = domain.dns_zone_resource_group_name
+        associated_endpoint_name     = endpoint.name
+      }
+    ]
+  ])
+}
---
+++ azure/cdn_frontdoor/main.tf
@@ -0,0 +1,133 @@
+resource "azurerm_cdn_frontdoor_profile" "profile" {
+  name                     = local.frontdoor_profile_name
+  resource_group_name      = var.resource_group_name
+  response_timeout_seconds = var.response_timeout_seconds
+  sku_name                 = local.sku_name[var.sku_name]
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_cdn_frontdoor_endpoint" "endpoint" {
+  for_each                 = { for endpoint in var.endpoints : lower(endpoint.name) => endpoint }
+  name                     = "fde-${each.value.name}-${var.environment}"
+  enabled                  = true
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.profile.id
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_cdn_frontdoor_origin_group" "group" {
+  for_each                 = { for endpoint in var.endpoints : lower(endpoint.name) => endpoint }
+  name                     = "fdog-${each.value.name}-${var.environment}"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.profile.id
+  session_affinity_enabled = each.value.origin_settings.session_affinity_enabled
+
+  restore_traffic_time_to_healed_or_new_endpoint_in_minutes = 10
+
+  health_probe {
+    interval_in_seconds = 30
+    path                = each.value.origin_settings.path
+    protocol            = "Https"
+    request_type        = "HEAD" #Do not change this as it might reduce the traffic load on the origin
+  }
+
+  load_balancing {
+    additional_latency_in_milliseconds = 1000
+    sample_size                        = 4
+    successful_samples_required        = 3
+  }
+}
+
+resource "azurerm_cdn_frontdoor_origin" "origin" {
+  for_each                       = { for origin in local.origins : "${lower(origin.fqdn)}-${origin.associated_endpoint_name}" => origin }
+  name                           = replace(replace(each.value.fqdn, ".", "-"), "\\.$", "")
+  cdn_frontdoor_origin_group_id  = azurerm_cdn_frontdoor_origin_group.group[lower(each.value.associated_endpoint_name)].id
+  enabled                        = true
+  certificate_name_check_enabled = false
+  host_name                      = each.value.fqdn
+  http_port                      = 80
+  https_port                     = 443
+  origin_host_header             = each.value.fqdn
+  # Calculate priority (incremental based on the order of the origin in the list)
+  # TODO: fix this priority as a dynamic value. Currently it will only work with one origin
+  priority = 1
+}
+
+resource "azurerm_cdn_frontdoor_rule_set" "rule_set" {
+  for_each                 = { for endpoint in var.endpoints : lower(endpoint.name) => endpoint }
+  name                     = each.value.name
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.profile.id
+}
+
+resource "azurerm_cdn_frontdoor_rule" "caching_rule" {
+  for_each = {
+    for endpoint in var.endpoints : lower(endpoint.name) => endpoint
+    if endpoint.caching_rule_enabled
+  }
+  name                      = each.value.name
+  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.rule_set[each.key].id
+  order                     = 1
+  behavior_on_match         = "Continue"
+
+  actions {
+    response_header_action {
+      header_action = "Delete"
+      header_name   = "Age"
+    }
+    response_header_action {
+      header_action = "Overwrite"
+      header_name   = "Cache-Control"
+      value         = "Max-Age=${60 * each.value.caching_timeout_minutes}"
+    }
+  }
+
+  depends_on = [azurerm_cdn_frontdoor_origin_group.group, azurerm_cdn_frontdoor_origin.origin]
+}
+
+resource "azurerm_cdn_frontdoor_custom_domain" "domain" {
+  for_each                 = { for domain in local.custom_domains : lower(domain.fqdn) => domain }
+  name                     = replace(replace(each.key, ".", "-"), "\\.$", "")
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.profile.id
+  dns_zone_id              = data.azurerm_dns_zone.dns_zone[each.key].id
+  host_name                = each.key
+
+  tls {
+    certificate_type    = "ManagedCertificate"
+    minimum_tls_version = "TLS12"
+  }
+}
+
+resource "azurerm_cdn_frontdoor_route" "route" {
+  for_each                      = { for endpoint in var.endpoints : lower(endpoint.name) => endpoint }
+  name                          = "fdr-${each.value.name}-${var.environment}"
+  cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.endpoint[each.key].id
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.group[each.key].id
+  cdn_frontdoor_origin_ids = compact([
+    for origin in azurerm_cdn_frontdoor_origin.origin :
+    origin.cdn_frontdoor_origin_group_id == azurerm_cdn_frontdoor_origin_group.group[each.key].id ? origin.id : null
+  ])
+
+  cdn_frontdoor_rule_set_ids = [azurerm_cdn_frontdoor_rule_set.rule_set[each.key].id]
+  enabled                    = true
+  cdn_frontdoor_origin_path  = each.value.origin_settings.path
+  forwarding_protocol        = "HttpsOnly"
+  https_redirect_enabled     = true
+  patterns_to_match          = each.value.origin_settings.patterns_to_match
+  supported_protocols        = ["Http", "Https"]
+
+  cdn_frontdoor_custom_domain_ids = compact([
+    for domain in local.custom_domains :
+    domain.associated_endpoint_name == each.key ? azurerm_cdn_frontdoor_custom_domain.domain[lower(domain.fqdn)].id : null
+  ])
+  link_to_default_domain = false
+
+  cache {
+    query_string_caching_behavior = "IgnoreQueryString"
+    compression_enabled           = false
+    content_types_to_compress     = []
+  }
+}
