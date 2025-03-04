a947a07019d1fb9511002d28ab96dce7de18f5cb
Filipe Figueredo Silva <filipe.silva@visma.com>
2025-03-04 00:33:40 +0000

    feature(domain): add azapi provider

--- azure/domain/terraform.tf
+++ azure/domain/terraform.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.117"
     }
+    
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>2.2.0"
+    }
   }
 
   required_version = ">= 1.5.0, < 2.0.0"
