fa6cf8855238c33784f12f29acc57c49e07e68b2
Filipe Figueredo Silva <filipe.silva@visma.com>
2025-03-05 00:10:00 +0000

    feature(security_group): add dynamic membership functionality

--- azure/security_group/main.tf
+++ azure/security_group/main.tf
@@ -2,4 +2,13 @@ resource "azuread_group" "security_group" {
   display_name     = var.name
   owners           = [data.azuread_client_config.current.object_id]
   security_enabled = true
+  types            = var.dynamic_membership_enabled ? ["DynamicMembership"] : []
+
+  dynamic "dynamic_membership" {
+    for_each = var.dynamic_membership_enabled ? [1] : []
+    content {
+      enabled = true
+      rule    = var.dynamic_membership_rule
+    }
+  }
 }
--- azure/security_group/variables.tf
+++ azure/security_group/variables.tf
@@ -2,3 +2,15 @@ variable "name" {
   description = "The name of the security group."
   type        = string
 }
+
+variable "dynamic_membership_enabled" {
+  description = "The name of the security group."
+  type        = bool
+  default     = false
+}
+
+variable "dynamic_membership_rule" {
+  description = "The name of the security group."
+  type        = string
+  default     = null
+}
