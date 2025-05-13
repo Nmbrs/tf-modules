562ad3a19f947d0c3ce2fff6209b0c1815ec4358
Pedro Luis <pedro.luis@visma.com>
2025-05-13 14:56:21 +0100

    comment provider

--- github/repositories/terraform.tf
+++ github/repositories/terraform.tf
@@ -9,7 +9,7 @@ terraform {
   required_version = ">= 1.5.0, < 2.0.0"
 }
 
-provider "github" {
-  owner = var.github_owner
-  token = var.github_token
-}
+#provider "github" {
+#  owner = var.github_owner
+#  token = var.github_token
+#}
