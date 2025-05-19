aae75872f1cbb7be16284765b97f2525e70f68ce
Filipe Figueredo Silva <filipe.silva@visma.com>
2025-05-19 13:38:15 +0100

    feature(github-organization): add organization module

---
+++ github/organization/variables.tf
@@ -0,0 +1,16 @@
+variable "name" {
+  type = string
+}
+
+variable "description" {
+  type = string
+}
+
+variable "billing_email" {
+  type = string
+}
+
+variable "url" {
+  type = string
+  default = null
+}
