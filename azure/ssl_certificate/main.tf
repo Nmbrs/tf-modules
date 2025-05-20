a78c03e61f1782792e9e3d533a72b051ac115dd8
Filipe Figueredo Silva <filipe.silva@visma.com>
2025-05-20 14:52:20 +0100

    feature(ssl-certificates): add override name parameter

--- azure/ssl_certificate/main.tf
+++ azure/ssl_certificate/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_app_service_certificate_order" "certificate" {
-  name                = local.certificate_name
+  name                = var.override_name != "" && var.override_name != null ? var.override_name : local.certificate_name
   resource_group_name = var.resource_group_name
   location            = "global"
   distinguished_name  = "CN=*.${var.domain_name}"
