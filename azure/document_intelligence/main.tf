34e39110887af99c9cd22449ed55dba23c8719b0
Pedro Luis <84625586+pmmluis@users.noreply.github.com>
2025-05-21 11:26:17 +0100

    fixed changed to allow autoscale and custom domain (#354)

--- azure/document_intelligence/main.tf
+++ azure/document_intelligence/main.tf
@@ -1,7 +1,17 @@
 resource "azurerm_cognitive_account" "document_intelligence" {
-  kind                = "FormRecognizer"
-  location            = var.location
-  name                = local.document_intelligence
-  resource_group_name = var.resource_group_name
-  sku_name            = "S0"
+  kind                       = "FormRecognizer"
+  location                   = var.location
+  name                       = local.document_intelligence
+  resource_group_name        = var.resource_group_name
+  sku_name                   = "S0"
+  custom_subdomain_name      = local.document_intelligence
+  dynamic_throttling_enabled = var.autoscale_enabled
+  network_acls {
+    default_action = var.network_acls.default_action
+    ip_rules       = var.network_acls.ip_rules
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
 }
