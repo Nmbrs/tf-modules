561e2f369dc8c1061413dede077288d1f520c9a3
Filipe Figueredo Silva <filipe.silva@visma.com>
2025-07-15 22:28:02 +0100

    fix(app_configuration): removed function operatin against null variable at naming logic

--- azure/app_configuration/local.tf
+++ azure/app_configuration/local.tf
@@ -1,6 +1,6 @@
 locals {
   name = (
-    var.override_name != null && trimspace(var.override_name) != "" ?
+    var.override_name != null && var.override_name != "" ?
     lower(var.override_name) :
     lower("appcs-${var.company_prefix}-${var.workload}-${var.environment}")
   )
