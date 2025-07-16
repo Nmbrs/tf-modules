5416d812650bdda7e752ed7d9650a80ecd98e08e
Filipe Figueredo Silva <filipe.silva@visma.com>
2025-07-16 17:15:11 +0100

    feature(storage_account): add network rules in deny mode

--- azure/storage_account/main.tf
+++ azure/storage_account/main.tf
@@ -1,13 +1,19 @@
 resource "azurerm_storage_account" "storage_account" {
-  name                          = local.storage_account_name
-  resource_group_name           = var.resource_group_name
-  location                      = var.location
-  account_kind                  = var.account_kind
-  account_tier                  = var.sku_name
-  account_replication_type      = var.replication_type
-  https_traffic_only_enabled    = true
-  min_tls_version               = "TLS1_2"
-  public_network_access_enabled = var.public_network_access_enabled
+  name                            = local.storage_account_name
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  account_kind                    = var.account_kind
+  account_tier                    = var.sku_name
+  account_replication_type        = var.replication_type
+  https_traffic_only_enabled      = true
+  min_tls_version                 = "TLS1_2"
+  public_network_access_enabled   = var.public_network_access_enabled
+  allow_nested_items_to_be_public = var.public_network_access_enabled
+
+network_rules {
+  default_action = "Deny"
+  bypass         = ["None"]
+}
 
   lifecycle {
     ignore_changes = [tags]
