4a402d73bec16c3276e4a42451867288b87ff6c8
Filipe Figueredo Silva <filipe.silva@visma.com>
2025-07-16 17:37:18 +0100

    feature(storage_account): add control for public network availability  (#366)
    * fix(storage_account): add public network logic
    
    * feature(storage_account): add network rules in deny mode
    
    * feature(storage_account): add network rules in deny mode

--- azure/storage_account/main.tf
+++ azure/storage_account/main.tf
@@ -1,14 +1,24 @@
 resource "azurerm_storage_account" "storage_account" {
-  name                       = local.storage_account_name
-  resource_group_name        = var.resource_group_name
-  location                   = var.location
-  account_kind               = var.account_kind
-  account_tier               = var.account_tier
-  account_replication_type   = var.replication_type
-  https_traffic_only_enabled = true
-  min_tls_version            = "TLS1_2"
+  name                            = local.storage_account_name
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  account_kind                    = var.account_kind
+  account_tier                    = var.sku_name
+  account_replication_type        = var.replication_type
+  https_traffic_only_enabled      = true
+  min_tls_version                 = "TLS1_2"
+  public_network_access_enabled   = var.public_network_access_enabled
+  allow_nested_items_to_be_public = var.public_network_access_enabled
+
+  network_rules {
+    default_action             = "Deny"
+    bypass                     = ["None"]
+    ip_rules                   = []
+    virtual_network_subnet_ids = []
+  }
 
   lifecycle {
     ignore_changes = [tags]
   }
+
 }
