ab1f5a2f2613f9fd55ca7b31a2ef70a1195e0231
Filipe Figueredo Silva <filipe.silva@visma.com>
2025-08-05 15:33:07 +0100

    feature(nat_gateway): enhance NAT gateway module with standardized patterns (#371)
    * feature(nat_gateway): enhance NAT gateway module with stnadardized patterns and advanced configuration
    
    * feature(nat_gateway): enhance NAT gateway module with stnadardized patterns and advanced configuration
    
    * feature(nat_gateway): enhance NAT gateway module with stnadardized patterns and advanced configuration
    
    * feature(nat_gateway): return with zone configuration

--- azure/nat_gateway/variables.tf
+++ azure/nat_gateway/variables.tf
@@ -1,44 +1,102 @@
-variable "resource_group_name" {
+variable "override_name" {
+  description = "Optional override for naming logic."
   type        = string
-  description = "The name of an existing Resource Group."
+  default     = null
+  nullable    = true
+
+  validation {
+    condition     = var.override_name == null || try(length(trimspace(var.override_name)) > 0, false)
+    error_message = format("Invalid value '%s' for variable 'override_name', it must be null or a non-empty string.", coalesce(var.override_name, "null"))
+  }
 }
 
 variable "workload" {
-  description = "Specifies the workload name which should be used for this NAT gateway."
+  description = "Short, descriptive name for the application, service, or workload. Used in resource naming conventions."
   type        = string
+  nullable    = true
+
+  validation {
+    condition     = var.workload == null || try(length(trimspace(var.workload)) > 0, false)
+    error_message = format("Invalid value '%s' for variable 'workload', it must be null or a non-empty string.", coalesce(var.workload, "null"))
+  }
+}
+
+variable "company_prefix" {
+  description = "Short, unique prefix for the company / organization."
+  type        = string
+  nullable    = true
+
+  validation {
+    condition     = var.company_prefix == null || try(length(trimspace(var.company_prefix)) > 0 && length(var.company_prefix) <= 5, false)
+    error_message = format("Invalid value '%s' for variable 'company_prefix', it must be a non-empty string with a maximum of 5 characters.", coalesce(var.company_prefix, "null"))
+  }
+}
+
+variable "sequence_number" {
+  description = "A numeric value used to ensure uniqueness for resource names."
+  type        = number
+  nullable    = true
+
+  validation {
+    condition     = var.sequence_number == null || try(var.sequence_number >= 1 && var.sequence_number <= 999, false)
+    error_message = format("Invalid value '%s' for variable 'sequence_number', it must be null or a number between 1 and 999.", coalesce(var.sequence_number, "null"))
+  }
 }
 
 variable "location" {
-  description = "The location where the resources will be deployed in Azure. For an exaustive list of locations, please use the command 'az account list-locations -o table'."
+  description = "Specifies Azure location where the resources should be provisioned. For an exhaustive list of locations, please use the command 'az account list-locations -o table'."
   type        = string
+  nullable    = false
+
+  validation {
+    condition     = length(trimspace(var.location)) > 0
+    error_message = format("Invalid value '%s' for variable 'location', it must be a non-empty string.", var.location)
+  }
 }
 
 variable "environment" {
+  description = "The environment in which the resource should be provisioned."
   type        = string
-  description = "Defines the environment to provision the resources."
-}
-
-variable "vnet_name" {
-  type        = string
-  description = "Name of the Vnet that will be added to the NAT gateway"
-}
-
-variable "vnet_resource_group_name" {
-  type        = string
-  description = "Resource group of the Vnet that will be added to the NAT gateway"
-}
-
-variable "subnets" {
-  type        = list(string)
-  description = "Subnets to be included in the NAT gateway"
-}
-
-variable "instance_count" {
-  type        = number
-  description = "A numeric sequence number used for naming the resource. It ensures a unique identifier for each resource instance in the naming convention."
+  nullable    = false
 
   validation {
-    condition     = var.instance_count >= 1 && var.instance_count <= 999
-    error_message = format("Invalid value '%s' for variable 'instance_count'. It must be between 1 and 999.", var.instance_count)
+    condition     = contains(["dev", "test", "prod", "sand", "stag"], var.environment)
+    error_message = format("Invalid value '%s' for variable 'environment', valid options are 'dev', 'test', 'prod', 'sand', 'stag'.", var.environment)
+  }
+}
+
+variable "resource_group_name" {
+  description = "Specifies the name of the resource group where the resource should be provisioned."
+  type        = string
+  nullable    = false
+
+  validation {
+    condition     = length(trimspace(var.resource_group_name)) > 0
+    error_message = format("Invalid value '%s' for variable 'resource_group_name', it must be a non-empty string.", var.resource_group_name)
+  }
+}
+
+variable "network_settings" {
+  description = "Settings related to the network connectivity of the NAT gateway."
+  type = object({
+    vnet_name                = string
+    vnet_resource_group_name = string
+    subnets                  = list(string)
+  })
+  nullable = false
+
+  validation {
+    condition     = try(length(trimspace(var.network_settings.vnet_name)) > 0, false)
+    error_message = format("Invalid value '%s' for variable 'network_settings.vnet_name', it must be a non-empty string.", coalesce(var.network_settings.vnet_name, "null"))
+  }
+
+  validation {
+    condition     = try(length(trimspace(var.network_settings.vnet_resource_group_name)) > 0, false)
+    error_message = format("Invalid value '%s' for variable 'network_settings.vnet_resource_group_name', it must be a non-empty string.", coalesce(var.network_settings.vnet_resource_group_name, "null"))
+  }
+
+  validation {
+    condition     = try(can(var.network_settings.subnets), false)
+    error_message = "Invalid value for variable 'network_settings.subnets', it must be a valid list."
   }
 }
