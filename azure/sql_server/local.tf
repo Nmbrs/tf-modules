b241cdbc8ad4f2a68c50935de82f1255beccf9af
Filipe Figueredo Silva <filipe.silva@visma.com>
2025-09-03 02:13:08 +0100

    feature(sql_server): allow location to the naming of the module

--- azure/sql_server/local.tf
+++ azure/sql_server/local.tf
@@ -2,7 +2,7 @@ locals {
   sql_server_name = (
     var.override_name != null && var.override_name != "" ?
     lower(var.override_name) :
-    lower("sqls-${var.company_prefix}-${var.workload}-${var.environment}-${format("%03d", var.sequence_number)}")
+    lower("sqls-${var.company_prefix}-${var.workload}-${var.environment}-${var.location}-${format("%03d", var.sequence_number)}")
   )
 
   audit_enabled = var.environment == "prod" || var.environment == "sand"
