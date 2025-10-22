6331f900805e0ab2fcc8c42354f0dd89b27b7948
Filipe Figueredo Silva <filipe.silva@visma.com>
2025-10-22 02:36:51 +0100

    feature(key_vault_hsm): enable soft delete and purge protection

--- azure/key_vault_hsm/main.tf
+++ azure/key_vault_hsm/main.tf
@@ -5,9 +5,8 @@ resource "azurerm_key_vault_managed_hardware_security_module" "main" {
   tenant_id           = data.azurerm_client_config.current.tenant_id
 
   sku_name                   = var.sku_name
-  # disabled for now because to perform tests or else we will be billed for the days we don't use the HSM
-  #soft_delete_retention_days = var.soft_delete_retention_days
-  #purge_protection_enabled   = var.purge_protection_enabled
+  soft_delete_retention_days = var.soft_delete_retention_days
+  purge_protection_enabled   = var.purge_protection_enabled
 
   admin_object_ids = [for group in data.azuread_group.admin_groups : group.object_id]
 
