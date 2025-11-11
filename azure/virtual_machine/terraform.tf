917de9d95a0adf70ebadec15e7366d63609b6f71
Filipe Figueredo Silva <filipe.silva@visma.com>
2025-11-11 13:49:48 +0000

    feature(virtual_machine): update module

--- azure/virtual_machine/terraform.tf
+++ azure/virtual_machine/terraform.tf
@@ -16,5 +16,5 @@ terraform {
     }
   }
 
-  required_version = ">= 1.3.0, < 2.0.0"
+  required_version = ">= 1.5.0, < 2.0.0"
 }
--- azure/virtual_machine/variables.tf
+++ azure/virtual_machine/variables.tf
@@ -71,11 +71,6 @@ variable "sku_name" {
   description = "The SKU which should be used for this Virtual Machine. For an exaustive list of virtual, please use the command 'az vm list-sizes --location 'your-location''."
   type        = string
   default     = "Standard_DS2_v2"
-
-  # validation {
-  #   condition     = contains(["Standard_DS2_v2", "Standard_D4as_v5", "Standard_D8as_v5", "Standard_F4s_v2", "Standard_F8s_v2", "Standard_F16s_v2"], var.vm_size)
-  #   error_message = format("Invalid value '%s' for variable 'sku_name', valid options are 'Standard_D2s_v2', 'Standard_D4as_v5', 'Standard_D8as_v5','Standard_F4s_v2', 'Standard_F8s_v2', 'Standard_F16s_v2'.", var.sku_name)
-  # }
 }
 
 variable "os_type" {
