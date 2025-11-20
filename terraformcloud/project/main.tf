a71af093307ac8d8547b25e0f1c044826243f27a
Filipe Figueredo Silva <filipe.silva@visma.com>
2025-11-20 12:43:32 +0000

    feature(terraformcloud-project): add agent support terraform cloud projects (#384)
    * feature(project): add agent pool support
    
    * feature(project): update tfe provider

--- terraformcloud/project/main.tf
+++ terraformcloud/project/main.tf
@@ -1,4 +1,4 @@
-resource "tfe_project" "project" {
+resource "tfe_project" "main" {
   name         = var.name
   organization = var.organization_name
 }
@@ -6,5 +6,12 @@ resource "tfe_project" "project" {
 resource "tfe_project_variable_set" "association" {
   for_each        = toset(var.associated_variable_sets)
   variable_set_id = data.tfe_variable_set.variable_set[each.key].id
-  project_id      = tfe_project.project.id
+  project_id      = tfe_project.main.id
+}
+
+resource "tfe_project_settings" "project" {
+  project_id = tfe_project.main.id
+
+  default_execution_mode = local.use_agent_pool ? "agent" : "remote"
+  default_agent_pool_id  = local.use_agent_pool ? data.tfe_agent_pool.agent_pool[0].id : null
 }
