b45f387dd8184f6418ee88ed5f81ee082aec5493
Filipe Figueredo Silva <filipe.silva@visma.com>
2025-12-10 16:58:34 +0000

    feature(key_vault_hsm): remove module (#397)

--- azure/key_vault_hsm/data.tf
+++
@@ -1,9 +0,0 @@
-data "azurerm_client_config" "current" {}
-
-# Fetch Azure AD security groups that will have admin access to the HSM
-data "azuread_group" "admin_groups" {
-  for_each = toset([for group in var.admin_group_names : lower(group)])
-
-  display_name     = each.value
-  security_enabled = true
-}
