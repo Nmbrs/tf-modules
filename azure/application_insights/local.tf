d383657498f0db34510be1481642a035dcf9c760
Filipe Figueredo Silva <filipe.silva@visma.com>
2026-03-16 23:40:33 +0000

    feature(application_insights): modernize module to conform with standard coding conventions

--- azure/application_insights/local.tf
+++ azure/application_insights/local.tf
@@ -1,3 +1,8 @@
 locals {
-  app_insights_name = "appi-${var.workload}-${var.environment}"
+  # Format: appi-{workload}-{env}
+  # Example: appi-contoso-prod
+  app_insights_name = (var.override_name != null ?
+    lower(var.override_name) :
+    lower("appi-${var.workload}-${var.environment}")
+  )
 }
